b972beda1b07b8e00065de2329759ea0f130a00c
Joo-Kwang Park <jookwang_park@tmax.co.kr>
2020-09-04 13:49:05 +0900

    feat: Ant Design 적용

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,20 @@
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
   babel: {
     plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
   },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
