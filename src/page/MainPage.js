21f03f4db6b2f17771fbedd50f52c288ea170bb9
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2020-09-17 09:08:24 +0900

    feat: main tab 추가 및 테스트 앱 추가

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,56 +1,41 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams, useHistory, useRouteMatch } from 'react-router-dom';
 import { Tabs, Button } from 'antd';
 import styled from 'styled-components';
-import Split from 'react-split';
 import { useCoreStores } from 'teespace-core';
-import { TeeTalk } from 'teespace-talk-app';
+// import { TeeTalk } from 'teespace-talk-app';
 // import { NoteApp } from 'teespace-note-app';
 // import { CalendarApp } from 'teespace-calendar-app';
+const { TabPane } = Tabs;
 
 const AppLayout = styled.div`
   display: flex;
-  flex-direction: column;
   width: 100%;
   height: 100%;
 `;
 
-const Header = styled.div`
+const LeftSide = styled.div`
   display: flex;
-  flex: 0 0 60px;
-  width: 100%;
+  height: 100%;
+  flex: 0 0 300px;
 `;
 
-const Content = styled.div`
+const Header = styled.div`
+  height: 60px;
+  background: yellow;
+`;
+
+const MainSide = styled.div`
   display: flex;
+  flex-direction: column;
   flex: 1;
   width: 100%;
 `;
 
-const Tab = styled.div`
+const AppContainer = styled.div`
   display: flex;
-  flex: 0 0 300px;
-  background: green;
+  flex: 1;
 `;
-
-const Title = styled.div`
-  display: flex;
-  flex: auto;
-  background: yellow;
-`;
-
-const AppSplitView = styled(Split)`
-  display: flex;
-  height: 100%;
-  flex: auto;
-  & .gutter {
-    border-right: 1px solid #e3e7eb;
-  }
-  & .gutter:hover {
-    cursor: ew-resize;
-  }
-`;
-
 const MainAppContainer = styled.div`
   display: flex;
   flex: auto;
@@ -74,99 +59,162 @@ function MainPage() {
   const params = useParams();
   const history = useHistory();
   const routeMatch = useRouteMatch();
-  const [layoutState, setLayoutState] = useState('collapse');
-  console.log('Params : ', params);
-  console.log('History : ', history);
-  console.log('RouteMatch : ', routeMatch);
+  const urlSearchParams = new URLSearchParams(history.location.search);
+  const [mainApp, setMainApp] = useState(params.mainApp);
+  const [subApp, setSubApp] = useState(null);
+
+  console.log(params);
+  console.log(history);
+  console.log(routeMatch);
+
+  // subApp useEffect
+  useEffect(() => {
+    setSubApp(urlSearchParams.get('sub'));
+  }, [history.location.search]);
+
+  // mainApp useEffect
+  useEffect(() => {
+    setMainApp(params.mainApp);
+  }, [params.mainApp]);
 
   const doLogout = useCallback(async () => {
     await authStore.logout();
     history.push('/login');
   }, [authStore, history]);
 
-  const renderContent = () => {
-    switch (layoutState) {
-      case 'full':
-        return <SubAppContainer>subapp</SubAppContainer>;
-        break;
-      case 'expand':
-        return (
-          <>
-            <Lnb></Lnb>
-            <SubAppContainer>subapp</SubAppContainer>
-          </>
-        );
-        break;
-      case 'collapse':
-        return (
-          <>
-            <Lnb></Lnb>
-            <AppSplitView
-              sizes={[75, 25]}
-              minSize={400}
-              expandToMin={false}
-              gutterSize={5}
-              gutterAlign="center"
-              snapOffset={10}
-              dragInterval={1}
-              direction="horizontal"
-              cursor="col-resize"
-            >
-              <MainAppContainer>mainapp</MainAppContainer>
-              <SubAppContainer>subapp</SubAppContainer>
-            </AppSplitView>
-          </>
-        );
-        break;
-      case 'close':
-        return (
-          <>
-            <Lnb></Lnb>
-            <MainAppContainer>mainapp</MainAppContainer>
-          </>
-        );
-        break;
+  const renderApp = isMainApp => {
+    const targetApp = isMainApp ? mainApp : subApp;
+    switch (targetApp) {
+      case 'talk':
+        return <Talk />;
+      case 'schedule':
+        return <Calendar />;
+      case 'drive':
+        return <Drive />;
+      case 'office':
+        return <Office />;
+      default:
+        return null;
     }
   };
 
   return (
     <AppLayout>
-      <Header>
-        <Tab></Tab>
-        <Title>
-          <button
+      <LeftSide>
+        <Tabs>
+          <TabPane key="1" tab="Friend">
+            <FriendLnb />
+          </TabPane>
+          <TabPane key="2" tab="Space">
+            <SpaceLnb />
+          </TabPane>
+          <TabPane key="3" tab="Mail">
+            <MailLnb />
+          </TabPane>
+        </Tabs>
+      </LeftSide>
+      <MainSide>
+        <Header>
+          <Button
+            type="button"
             onClick={() => {
-              setLayoutState('full');
+              history.push({
+                pathname: `/${params['0']}/${params.id}/${params.mainApp}`,
+                search: '?sub=schedule',
+              });
             }}
           >
-            전체
-          </button>
-          <button
+            서브 스케줄
+          </Button>
+          <Button
+            type="button"
             onClick={() => {
-              setLayoutState('expand');
+              history.push({
+                pathname: `/${params['0']}/${params.id}/${params.mainApp}`,
+                search: '?sub=office',
+              });
             }}
           >
-            확장
-          </button>
-          <button
+            서브 오피스
+          </Button>
+          <Button
+            type="button"
             onClick={() => {
-              setLayoutState('collapse');
+              history.push({
+                pathname: `/${params['0']}/${params.id}/drive`,
+                search: history.location.search,
+              });
             }}
           >
-            축소
-          </button>
-          <button
+            메인 드라이브
+          </Button>
+          <Button
+            type="button"
             onClick={() => {
-              setLayoutState('close');
+              history.push({
+                pathname: `/${params['0']}/${params.id}/talk`,
+                search: history.location.search,
+              });
             }}
           >
-            닫기
-          </button>
-        </Title>
-      </Header>
-      <Content>{renderContent()}</Content>
+            메인 토크
+          </Button>
+        </Header>
+        <AppContainer>
+          <MainAppContainer>{renderApp(true)}</MainAppContainer>
+          <SubAppContainer>{renderApp(false)}</SubAppContainer>
+        </AppContainer>
+      </MainSide>
     </AppLayout>
   );
 }
+
+const Talk = () => {
+  return (
+    <div>
+      Talk App
+      <input type="text" style={{ height: '30px' }} />
+    </div>
+  );
+};
+
+const FriendLnb = () => {
+  return <div>Friend LNB</div>;
+};
+
+const SpaceLnb = () => {
+  return <div>Space LNB</div>;
+};
+
+const MailLnb = () => {
+  return <div>Mail LNB</div>;
+};
+
+const Calendar = () => {
+  return (
+    <div>
+      Schedule App
+      <input type="text" style={{ height: '30px' }} />
+    </div>
+  );
+};
+
+const Drive = () => {
+  return (
+    <div>
+      Drive App
+      <input type="text" style={{ height: '30px' }} />
+    </div>
+  );
+};
+
+const Office = () => {
+  return (
+    <div>
+      Office App
+      <input type="text" style={{ height: '30px' }} />
+    </div>
+  );
+};
 
 export default MainPage;
