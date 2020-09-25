f6f796754759e217667d1fdfdc9b0c69eb4c2f9e
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2020-09-25 13:48:45 +0900

    feat: note와 layoutState 및 eventBus 연동

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,8 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useParams, useHistory, useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useOpenInWindow } from 'use-open-window';
 import { Tabs } from 'antd';
 import styled from 'styled-components';
-import { useCoreStores, EventBus } from 'teespace-core';
+import { useCoreStores, EventBus, WWMS } from 'teespace-core';
 import { Talk } from 'teespace-talk-app';
 import { NoteApp, NoteIcon } from 'teespace-note-app';
 import { CalendarApp, CalendarIcon } from 'teespace-calendar-app';
@@ -17,20 +18,78 @@ import './mainPage.css';
 const { TabPane } = Tabs;
 
 const DEFAULT_MAIN_APP = 'talk';
-const eventBus = EventBus;
 
 function MainPage() {
-  const { authStore } = useCoreStores();
+  const coreStore = useCoreStores();
+  console.log(coreStore);
   const params = useParams();
   const history = useHistory();
   const [tabType, setTabType] = useState(null);
   const [mainApp, setMainApp] = useState(null);
   const [subApp, setSubApp] = useState(null);
   const [layoutState, setLayoutState] = useState('close');
+  const [isNewWindow, setIsNewWindow] = useState(false);
+
+  // NEW WINDOW TEST
+  const [handleTalkWindowOpen, newTalkWindowHandler] = useOpenInWindow(
+    `${window.location.origin}/s/1234/talk?mini=true`,
+    {
+      name: '_blank',
+      centered: true,
+      specs: {
+        width: 600,
+        height: 900,
+      },
+    },
+  );
+
+  // NEW WINDOW TEST
+  const [handleNoteWindowOpen, newNoteWindowHandler] = useOpenInWindow(
+    `${window.location.origin}/s/1234/note?mini=true`,
+    {
+      name: '_blank',
+      centered: true,
+      specs: {
+        width: 1024,
+        height: 768,
+      },
+    },
+  );
+
+  // console.log(params);
+  // console.log(history);
+
+  // useEffect(() => {
+  //   WWMS.setConfig({
+  //     url: 'wss://echo.websocket.org',
+  //     isDebug: true,
+
+  //     useInterval: false,
+  //     intervalTime: 1000,
+
+  //     useReconnect: false,
+  //     reconnectInterval: 2000,
+
+  //     intervalFunction: () => {
+  //       console.log('send ping.');
+  //     },
+
+  //     onopen: null,
+  //     onerror: null,
+  //     onmessage: null,
+  //     onclose: null,
+  //   });
+
+  //   WWMS.addHandler('CHN0001', msg => {
+  //     alert(`[WWMS received] : ${msg.TEXT}`);
+  //   });
+  //   WWMS.connect();
+  // }, []);
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(history.location.search);
     const subAppQuery = urlSearchParams.get('sub');
+    setIsNewWindow(urlSearchParams.has('mini'));
 
     setTabType(params['0']);
     setMainApp(params.mainApp);
@@ -43,16 +102,16 @@ function MainPage() {
   }, [params, history, layoutState]);
 
   useEffect(() => {
-    const fullHandleId = eventBus.on('onLayoutFull', param => {
+    const fullHandleId = EventBus.on('onLayoutFull', param => {
       setLayoutState('full');
     });
-    const expandHandleId = eventBus.on('onLayoutExpand', param => {
+    const expandHandleId = EventBus.on('onLayoutExpand', param => {
       setLayoutState('expand');
     });
-    const collapseHandleId = eventBus.on('onLayoutCollapse', param => {
+    const collapseHandleId = EventBus.on('onLayoutCollapse', param => {
       setLayoutState('collapse');
     });
-    const closeHandleId = eventBus.on('onLayoutClose', param => {
+    const closeHandleId = EventBus.on('onLayoutClose', param => {
       setLayoutState('close');
       history.push({
         pathname: history.location.pathname,
@@ -61,31 +120,36 @@ function MainPage() {
     });
 
     return function cleanUp() {
-      eventBus.off('onLayoutFull', fullHandleId);
-      eventBus.off('onLayoutExpand', expandHandleId);
-      eventBus.off('onLayoutCollapse', collapseHandleId);
-      eventBus.off('onLayoutClose', closeHandleId);
+      EventBus.off('onLayoutFull', fullHandleId);
+      EventBus.off('onLayoutExpand', expandHandleId);
+      EventBus.off('onLayoutCollapse', collapseHandleId);
+      EventBus.off('onLayoutClose', closeHandleId);
     };
   }, []);
 
-  const renderApp = isMainApp => {
-    const targetApp = isMainApp ? mainApp : subApp;
-
-    switch (targetApp) {
+  const renderApp = app => {
+    switch (app) {
+      case 'profile':
+        // TODO : Profile Component 받기.
+        return <Profile />;
       case 'talk':
-        return <Talk />;
+        return <Talk layoutState={layoutState} />;
       case 'note':
-        return <NoteApp />;
+        return <NoteApp layoutState={layoutState} />;
       case 'schedule':
-        return <CalendarApp />;
+        return <CalendarApp layoutState={layoutState} />;
       case 'drive':
-        return <DriveApp />;
+        return <DriveApp layoutState={layoutState} />;
       case 'plus':
-        return <DriveApp />;
-      case 'office':
-        return null;
+        return <DriveApp layoutState={layoutState} />;
       case 'mail':
-        return <MailMainView />;
+        return <MailMainView layoutState={layoutState} />;
+      case 'office':
+        // TODO : Office Component 받기.
+        return null;
+      case 'Meeting':
+        // TODO : Meeting Component 받기.
+        return null;
       default:
         return null;
     }
@@ -97,7 +161,7 @@ function MainPage() {
     switch (key) {
       /* friend : /f/:id 형식 (query string, app 정보 없음) */
       case 'f':
-        pathname = `/${key}/${params.id}`;
+        pathname = `/${key}/${params.id}/profile`;
         search = null;
         break;
       /* space, mail : /f/:id/:app?sub... 형식  */
@@ -121,177 +185,147 @@ function MainPage() {
 
   return (
     <AppLayout>
-      <LeftSide>
-        <Tabs activeKey={tabType} onTabClick={handleTabClick}>
-          <TabPane
-            key="f"
-            tab={
-              <img src={friendIcon} alt="friends" style={{ width: '40px' }} />
-            }
-          >
-            <FriendLnb />
-          </TabPane>
+      {!isNewWindow ? (
+        <>
+          <LeftSide>
+            <Tabs activeKey={tabType} onTabClick={handleTabClick}>
+              <TabPane
+                key="f"
+                tab={
+                  <img
+                    src={friendIcon}
+                    alt="friends"
+                    style={{ width: '40px' }}
+                  />
+                }
+              >
+                <FriendLnb />
+              </TabPane>
 
-          <TabPane
-            key="s"
-            tab={<img src={chatIcon} alt="chat" style={{ width: '40px' }} />}
-          >
-            <SpaceLnb />
-          </TabPane>
+              <TabPane
+                key="s"
+                tab={
+                  <img src={chatIcon} alt="chat" style={{ width: '40px' }} />
+                }
+              >
+                <SpaceLnb />
+              </TabPane>
 
-          <TabPane
-            key="m"
-            tab={<img src={mailIcon} alt="mail" style={{ width: '30px' }} />}
-          >
-            <MailSideView />
-          </TabPane>
-        </Tabs>
-      </LeftSide>
-      <MainSide>
-        <Header>
-          <Title>
-            Title 영역 (icon container에 따라 가변)
-            <button
-              type="button"
-              style={{
-                display: !(subApp && layoutState !== 'full') ? 'none' : 'block',
-              }}
-              onClick={() => {
-                eventBus.dispatch('onLayoutFull', {
-                  isMaximize: true,
-
-                  callback: param => {
-                    console.log('NOTE: onLayoutFull callback : ', param);
-                  },
-                });
-              }}
-            >
-              전체
-            </button>
-            <button
-              type="button"
-              style={{
-                display: !(subApp && layoutState !== 'expand')
-                  ? 'none'
-                  : 'block',
-              }}
-              onClick={() => {
-                eventBus.dispatch('onLayoutExpand', {
-                  isMaximize: true,
-
-                  callback: param => {
-                    console.log('NOTE: onLayoutFull callback : ', param);
-                  },
-                });
-              }}
-            >
-              확장
-            </button>
-            <button
-              type="button"
-              style={{
-                display: !(subApp && layoutState !== 'collapse')
-                  ? 'none'
-                  : 'block',
-              }}
-              onClick={() => {
-                eventBus.dispatch('onLayoutCollapse', {
-                  isMaximize: true,
-
-                  callback: param => {
-                    console.log('NOTE: onLayoutCollapse callback : ', param);
-                  },
-                });
-              }}
-            >
-              축소
-            </button>
-            <button
-              type="button"
-              style={{
-                display: !(subApp && layoutState !== 'close')
-                  ? 'none'
-                  : 'block',
-              }}
-              onClick={() => {
-                eventBus.dispatch('onLayoutClose', {
-                  isMaximize: true,
-
-                  callback: param => {
-                    console.log('NOTE: onLayoutClose callback : ', param);
-                  },
-                });
-              }}
-            >
-              닫기
-            </button>
-          </Title>
-          <AppIconContainer>
-            <NoteIcon
-              width={50}
-              height={50}
-              state={subApp === 'note' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: `?sub=note`,
-                });
-              }}
-            />
-            <DriveIcon
-              width={50}
-              height={50}
-              state={subApp === 'drive' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: `?sub=drive`,
-                });
-              }}
-            />
-            <CalendarIcon
-              width={50}
-              height={50}
-              state={subApp === 'schedule' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: `?sub=schedule`,
-                });
-              }}
-            />
-            <ViewFileIcon
-              width={50}
-              height={50}
-              state={subApp === 'plus' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: `?sub=plus`,
-                });
-              }}
-            />
-          </AppIconContainer>
-          <Profile>Profile 영역 (고정)</Profile>
-        </Header>
-        <AppContainer>
-          <Splitter
-            sizes={[75, 25]}
-            minSize={400}
-            gutterSize={10}
-            layoutState={layoutState}
-          >
-            <MainAppContainer>{renderApp(true)}</MainAppContainer>
-            <SubAppContainer>{renderApp(false)}</SubAppContainer>
-          </Splitter>
-        </AppContainer>
-      </MainSide>
+              <TabPane
+                key="m"
+                tab={
+                  <img src={mailIcon} alt="mail" style={{ width: '30px' }} />
+                }
+              >
+                <MailSideView />
+              </TabPane>
+            </Tabs>
+          </LeftSide>
+          <MainSide>
+            <Header>
+              <Title>
+                Title 영역 (icon container에 따라 가변)
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log(handleTalkWindowOpen());
+                  }}
+                >
+                  Talk 새창
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log(handleNoteWindowOpen());
+                  }}
+                >
+                  Note 새창
+                </button>
+                {/* <button
+                  type="button"
+                  onClick={() => {
+                    WWMS.send(
+                      JSON.stringify({ CH_TYPE: 'CHN0001', TEXT: '하이' }),
+                    );
+                  }}
+                >
+                  Websocket TEST
+                </button> */}
+              </Title>
+              <AppIconContainer>
+                <NoteIcon
+                  width={50}
+                  height={50}
+                  state={subApp === 'note' ? 'active' : 'default'}
+                  onClick={() => {
+                    history.push({
+                      pathname: history.location.pathname,
+                      search: `?sub=note`,
+                    });
+                  }}
+                />
+                <DriveIcon
+                  width={50}
+                  height={50}
+                  state={subApp === 'drive' ? 'active' : 'default'}
+                  onClick={() => {
+                    history.push({
+                      pathname: history.location.pathname,
+                      search: `?sub=drive`,
+                    });
+                  }}
+                />
+                <CalendarIcon
+                  width={50}
+                  height={50}
+                  state={subApp === 'schedule' ? 'active' : 'default'}
+                  onClick={() => {
+                    history.push({
+                      pathname: history.location.pathname,
+                      search: `?sub=schedule`,
+                    });
+                  }}
+                />
+                <ViewFileIcon
+                  width={50}
+                  height={50}
+                  state={subApp === 'plus' ? 'active' : 'default'}
+                  onClick={() => {
+                    history.push({
+                      pathname: history.location.pathname,
+                      search: `?sub=plus`,
+                    });
+                  }}
+                />
+              </AppIconContainer>
+              <UserMenu>Profile 영역 (고정)</UserMenu>
+            </Header>
+            <AppContainer>
+              <Splitter
+                sizes={[75, 25]}
+                minSize={400}
+                gutterSize={10}
+                layoutState={layoutState}
+              >
+                <MainAppContainer>{renderApp(mainApp)}</MainAppContainer>
+                <SubAppContainer>{renderApp(subApp)}</SubAppContainer>
+              </Splitter>
+            </AppContainer>
+          </MainSide>
+        </>
+      ) : (
+        renderApp(mainApp)
+      )}
     </AppLayout>
   );
 }
 
 const FriendLnb = () => {
   return <div>Friend LNB</div>;
+};
+
+const Profile = ({ userId }) => {
+  return <div>{`${userId}의 profile`}</div>;
 };
 
 const SpaceLnb = () => {
@@ -307,7 +341,7 @@ const AppLayout = styled.div`
 const LeftSide = styled.div`
   display: flex;
   height: 100%;
-  width: 300px;
+  width: 260px;
   border-right: 1px solid #dddddd;
 `;
 
@@ -335,14 +369,14 @@ const AppIconContainer = styled.div`
   padding: 0 15px;
 `;
 
-const Profile = styled.div`
+const UserMenu = styled.div`
   padding: 0 10px;
 `;
 
 const MainSide = styled.div`
   display: flex;
   flex-direction: column;
-  width: calc(100% - 300px);
+  width: calc(100% - 260px);
   height: 100%;
 `;
 
