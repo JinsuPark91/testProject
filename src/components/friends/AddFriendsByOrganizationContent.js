96838c1f0c8578859b8548059987c0bf8cab63e6
junkyu_lee <junkyu_lee@tmax.co.kr>
2020-10-12 15:54:07 +0900

    feat(friends): 조직도 선택시  불러오기 추가

--- src/components/friends/AddFriendsByOrganizationContent.js
+++ src/components/friends/AddFriendsByOrganizationContent.js
@@ -1,71 +1,27 @@
 import React from 'react';
+import { useObserver } from 'mobx-react';
+import { useCoreStores } from 'teespace-core';
 import { Row, Col, Typography } from 'antd';
 import FriendItem from './FriendItem';
 
 const { Text } = Typography;
 
-const dummy = [
-  {
-    id: 1,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 2,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 3,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 4,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 5,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 6,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 7,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-  {
-    id: 8,
-    userName: '이준규',
-    team: 'AC1-2팀',
-    position: '팀원',
-  },
-];
 function AddFriendsByOrganizationContent() {
-  return (
+  const { orgStore } = useCoreStores();
+  return useObserver(() => (
     <Row align="middle" style={{ flexGrow: 1 }} justify="center">
       <Col span={24}>
-        {true &&
-          dummy.map(item => (
-            <FriendItem friendInfo={item} key={item.id} mode="addFriend" />
-          ))}
+        {orgStore.orgUserList.map(item => (
+          <FriendItem
+            friendInfo={{ userName: item.loginId, friendId: item.id }}
+            key={item.id}
+            mode="addFriend"
+          />
+        ))}
         {false && <Text type="secondary">조직원이 없습니다.</Text>}
       </Col>
     </Row>
-  );
+  ));
 }
 
 export default AddFriendsByOrganizationContent;
