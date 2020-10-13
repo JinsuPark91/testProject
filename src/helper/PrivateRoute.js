38ce480b4e90c4f5781ead392dfa6c0fdd672a0e
jinsu_park <jinsu_park@tmax.co.kr>
2020-10-13 15:38:11 +0900

    feat: wwms 적용 - privateRoute.js 최초 호출시에만 셋팅

--- src/helper/PrivateRoute.js
+++ src/helper/PrivateRoute.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { useCoreStores } from 'teespace-core';
+import { useCoreStores, WWMS } from 'teespace-core';
 
 export default function PrivateRoute({ component, ...rest }) {
   const { authStore } = useCoreStores();
+  const [hasWWMSConfig, setHasWWMSConfig] = useState(null);
+  // 테스트용 에코서버
+  const echoServer = 'wss://echo.websocket.org';
+
+  if (hasWWMSConfig === null) {
+    WWMS.setConfig({
+      // url: `${process.env.REACT_APP_WEBSOCKET_URL}?USER_ID=${authStore.myInfo.id}&action=&CONNECTION_ID=undefined`,
+      url: echoServer,
+      isDebug: true,
+
+      useInterval: false,
+      intervalTime: 1000,
+
+      useReconnect: true,
+      reconnectInterval: 2000,
+
+      intervalFunction: () => {
+        console.log('send ping.');
+      },
+      onopen: null,
+      onerror: null,
+      onmessage: null,
+      onclose: null,
+    });
+    WWMS.connect();
+    setHasWWMSConfig(true);
+  }
 
   return (
     <Route
