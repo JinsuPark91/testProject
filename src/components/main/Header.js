81e1498f22522c179ba4ab695450811fec794442
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2020-11-10 10:55:06 +0900

    refactor: header code 간소화

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Observer } from 'mobx-react';
 import { useHistory } from 'react-router-dom';
 import { NoteIcon } from 'teespace-note-app';
@@ -7,74 +7,47 @@ import { CalendarIcon } from 'teespace-calendar-app';
 import { Wrapper, Title, AppIconContainer, UserMenu } from './HeaderStyle';
 import PlatformUIStore from '../../stores/PlatformUIStore';
 
+const apps = ['note', 'drive', 'calendar', 'files'];
+
 const Header = () => {
   const history = useHistory();
 
+  const handleAppClick = appName => {
+    history.push({
+      pathname: history.location.pathname,
+      search: `?sub=${appName}`,
+    });
+  };
+
+  const getAppIcon = appName => {
+    const props = {
+      key: appName,
+      state: PlatformUIStore.subApp === appName ? 'active' : 'default',
+      onClick: () => handleAppClick(appName),
+    };
+
+    switch (appName) {
+      case 'note':
+        return <NoteIcon {...props} />;
+      case 'drive':
+        return <DriveIcon {...props} />;
+      case 'calendar':
+        return <CalendarIcon {...props} />;
+      case 'files':
+        return <ViewFileIcon {...props} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Wrapper>
-      {/* 
-          useObsever 사용 시 header 전체를 다시 그리는 반면,  
-          Observer 사용 시, Title 부분만 다시 그릴수 있다.
-      */}
       <Observer>
         {() => <Title>{`${PlatformUIStore.resourceId}`}</Title>}
       </Observer>
 
       <AppIconContainer>
-        <Observer>
-          {() => (
-            <NoteIcon
-              state={PlatformUIStore.subApp === 'note' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: '?sub=note',
-                });
-              }}
-            />
-          )}
-        </Observer>
-        <Observer>
-          {() => (
-            <DriveIcon
-              state={PlatformUIStore.subApp === 'drive' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: '?sub=drive',
-                });
-              }}
-            />
-          )}
-        </Observer>
-        <Observer>
-          {() => (
-            <CalendarIcon
-              state={
-                PlatformUIStore.subApp === 'calendar' ? 'active' : 'default'
-              }
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: '?sub=calendar',
-                });
-              }}
-            />
-          )}
-        </Observer>
-        <Observer>
-          {() => (
-            <ViewFileIcon
-              state={PlatformUIStore.subApp === 'files' ? 'active' : 'default'}
-              onClick={() => {
-                history.push({
-                  pathname: history.location.pathname,
-                  search: '?sub=files',
-                });
-              }}
-            />
-          )}
-        </Observer>
+        <Observer>{() => apps.map(appName => getAppIcon(appName))}</Observer>
       </AppIconContainer>
 
       <UserMenu>
