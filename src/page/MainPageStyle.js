1329a7307f6e78b1afd776665cd0e3198ba82117
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2020-11-16 14:55:45 +0900

    feat: loader 추가, room item dropdown styling

--- src/page/MainPageStyle.js
+++ src/page/MainPageStyle.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
+export const Loader = styled.div`
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
+
+    & img{
+      width: 5rem;
+      height: auto;
+    }
+}`;
+
 export const Wrapper = styled.div`
   display: flex;
   width: 100%;
