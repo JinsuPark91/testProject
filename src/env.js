f1b638e3b73a0a3b6fc21e0c3debad0390d5a243
DongHee Yang <donghee.yang@samsung.com>
2020-12-10 15:08:03 +0900

    feat: add resourceURL to environment for specifying photo URL

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 const env = {
   serviceURL: null,
+  resourceURL: null,
   websocketURL: null,
 };
 
@@ -7,7 +8,12 @@ export const getEnv = () => {
   return env;
 };
 
-export const setEnv = ({ serviceURL = null, websocketURL = null }) => {
+export const setEnv = ({
+  serviceURL = null,
+  resourceURL = null,
+  websocketURL = null,
+}) => {
   env.serviceURL = serviceURL || env.serviceURL;
+  env.resourceURL = resourceURL || env.resourceURL;
   env.websocketURL = websocketURL || env.websocketURL;
 };
