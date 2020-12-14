d98249eb670153c70055299e2c0399e9e9c23203
bin_lim <bin_lim@tmax.co.kr>
2020-12-14 17:41:11 +0900

    feat(keycloak-config): clientId를 config.js에서 설정할 수 있도록 변경

--- src/libs/keycloak.js
+++ src/libs/keycloak.js
@@ -4,7 +4,7 @@ const keycloakConfig = {
 
   url:  window.env.REACT_APP_HYPERAUTH_URL || process.env.REACT_APP_HYPERAUTH_URL, // keycloak server 주소
   // realm: 'HyperSpace', // keycloak admin에서 생성한 realm 이름
-  clientId: 'hyperspace', // keycloak admin에서 해당 app과 연결한 clientId
+  clientId: window.env.REACT_APP_HYPERAUTH_CLIENT_ID || process.env.REACT_APP_HYPERAUTH_CLIENT_ID, // keycloak admin에서 해당 app과 연결한 clientId
   // url: 'http://localhost:8080/auth',
   // realm: 'demo', // keycloak admin에서 생성한 realm 이름
   // clientId: 'vanila', // keycloak admin에서 해당 app과 연결한 clientId
