7029c6cb0b49167d93268da5b12085b3f8d776d8
bin_lim <bin_lim@tmax.co.kr>
2020-12-15 16:50:14 +0900

    feat(keycloak-config): 키클록 환경 변수 config.js에 설정

--- src/libs/keycloak.js
+++ src/libs/keycloak.js
@@ -1,10 +1,17 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
-
-  url:  window.env.REACT_APP_HYPERAUTH_URL || process.env.REACT_APP_HYPERAUTH_URL, // keycloak server 주소
+  url:
+    window.env.REACT_APP_HYPERAUTH_URL === '%HYPERAUTH_URL%'
+      ? process.env.REACT_APP_HYPERAUTH_URL
+      : window.env.REACT_APP_HYPERAUTH_URL ||
+        process.env.REACT_APP_HYPERAUTH_URL, // keycloak server 주소
   // realm: 'HyperSpace', // keycloak admin에서 생성한 realm 이름
-  clientId: window.env.REACT_APP_HYPERAUTH_CLIENT_ID || process.env.REACT_APP_HYPERAUTH_CLIENT_ID, // keycloak admin에서 해당 app과 연결한 clientId
+  clientId:
+    window.env.REACT_APP_HYPERAUTH_CLIENT_ID === '%HYPERAUTH_CLIENT_ID%'
+      ? process.env.REACT_APP_HYPERAUTH_CLIENT_ID
+      : window.env.REACT_APP_HYPERAUTH_CLIENT_ID ||
+        process.env.REACT_APP_HYPERAUTH_CLIENT_ID, // keycloak admin에서 해당 app과 연결한 clientId
   // url: 'http://localhost:8080/auth',
   // realm: 'demo', // keycloak admin에서 생성한 realm 이름
   // clientId: 'vanila', // keycloak admin에서 해당 app과 연결한 clientId
