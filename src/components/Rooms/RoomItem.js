55cef9f2231e206230f6cb2424658926d4f404e1
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2020-12-16 17:47:29 +0900

    chore: room accept item 추가

--- src/components/Rooms/RoomItem.js
+++ src/components/Rooms/RoomItem.js
@@ -325,6 +325,8 @@ const ACCEPT_ITEMS = [
   'Item:Drive:Files',
   'Item:Note:Pages',
   'Item:Note:SharedPages',
+  'Item:Note:Chapters',
+  'Item:Note:SharedChapters',
   'Item:Calendar:ShareSchedules',
 ];
 const TALK_ACCEPT_ITEMS = [
