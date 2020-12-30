a3c3c06d35a6ce7799daff2911b0cf118f8d4ea2
bin_lim <bin_lim@tmax.co.kr>
2020-12-30 06:47:42 +0000

    Merge branch 'develop' into 'feature-keycloak'
    Develop
    
    See merge request teespace/teespace-platform!137

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import {
 } from 'teespace-calendar-app';
 import { initApp as initMailApp } from 'teespace-mail-app';
 // import { I18nextProvider } from 'react-i18next';
-import { useKeycloak } from '@react-keycloak/web';
 import AdminPage from './page/AdminPage';
 import LoginPage from './page/LoginPage';
 import NotFoundPage from './page/NotFoundPage';
@@ -28,14 +27,38 @@ import MainPage from './page/MainPage';
 import RedirectablePublicRoute from './libs/RedirectablePublicRoute';
 import PrivateRoute from './libs/PrivateRoute';
 import KeycloakRedirectRoute from './libs/KeycloakRedirectRoute';
+import keycloak from './libs/keycloak';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 // import i18next from './i18n';
 
 const hydrate = create();
 
 function App() {
   const [isHydrating, setIsHydrating] = useState(false);
-  const { initialized } = useKeycloak();
   const { authStore, userStore } = useCoreStores();
+
+  const url = window.location.origin; //  http://xxx.dev.teespace.net
+  const con_url = url.split(`//`)[1]; // xxx.dev.teespace.net
+  const sub_url = url.split(`//`)[1].split(`.`)[0]; //  xxx
+  const main_url = con_url.slice(con_url.indexOf('.') + 1, con_url.length); // dev.teespace.net
+
+  const getCookieValue = key => {
+    const cookieKey = `${key}=`;
+    let result = '';
+    const cookieArr = document.cookie.split(';');
+
+    for (let i = 0; i < cookieArr.length; i++) {
+      if (cookieArr[i][0] === ' ') {
+        cookieArr[i] = cookieArr[i].substring(1);
+      }
+
+      if (cookieArr[i].indexOf(cookieKey) === 0) {
+        result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
+        return result;
+      }
+    }
+    return result;
+  };
 
   // initialize apps
   useEffect(() => {
@@ -48,9 +71,6 @@ function App() {
 
   // hydrate mobx stores
   useEffect(() => {
-    if (!initialized) {
-      return;
-    }
     Promise.all([hydrate('auth', authStore), hydrate('user', userStore)])
       .then(() => {
         userStore.initHydratedMyProfile({});
@@ -58,62 +78,94 @@ function App() {
       })
       .catch(e => console.error(e));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialized]);
+  }, []);
 
-  if (!isHydrating || !initialized) return <></>;
+  if (!isHydrating) return <></>;
   return (
     <DndProvider backend={HTML5Backend}>
-      <PortalProvider>
-        {/* <I18nextProvider i18n={i18next}> */}
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/login" />
-            </Route>
-            <Route exact path="/logout" component={LogoutPage} />
-            <Route exact path="/drive/files/:fileId">
-              <DriveSharedFilePage />
-            </Route>
-            <KeycloakRedirectRoute
-              exact
-              path="/login"
-              component={
-                process.env.REACT_APP_ENV === `local` ? LoginPage : MainPage
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/drive/files/:fileId">
+            <DriveSharedFilePage />
+          </Route>
+          <Route>
+            <ReactKeycloakProvider
+              authClient={keycloak}
+              LoadingComponent={<></>}
+              initOptions={
+                // tokenParam
+                getCookieValue(`ACCESS_TOKEN`)
+                  ? {
+                      token: getCookieValue(`ACCESS_TOKEN`),
+                      refreshToken: getCookieValue(`REFRESH_TOKEN`),
+                    }
+                  : {
+                      onLoad: 'check-sso',
+                      checkLoginIframe: false,
+                      redirectUri:
+                        process.env.REACT_APP_ENV === 'local'
+                          ? `${window.location.href}`
+                          : `${window.location.protocol}//${main_url}/domain/${sub_url}`,
+                    }
               }
-            />
-            <PrivateRoute path="/office/:fileId" component={OfficeFilePage} />
-            <RedirectablePublicRoute
-              exact
-              path="/register"
-              component={<SignUpPage />}
-            />
-            <RedirectablePublicRoute
-              exact
-              path="/registerForm"
-              component={<SignUpFormPage />}
-            />
-            <RedirectablePublicRoute
-              exact
-              path="/registerComplete"
-              component={<SignUpCompletePage />}
-            />
-            <PrivateRoute
-              path="/:resourceType(s|f|m)/:resourceId/:mainApp?"
-              component={MainPage}
-            />
-            <Route path="/admin">
-              <AdminPage />
-            </Route>
+            >
+              <PortalProvider>
+                {/* <I18nextProvider i18n={i18next}> */}
+                <BrowserRouter>
+                  <Switch>
+                    <Route exact path="/">
+                      <Redirect to="/login" />
+                    </Route>
+                    <Route exact path="/logout" component={LogoutPage} />
 
-            <Route component={NotFoundPage} />
-            {/* <PrivateRoute
+                    <KeycloakRedirectRoute
+                      exact
+                      path="/login"
+                      component={
+                        process.env.REACT_APP_ENV === `local`
+                          ? LoginPage
+                          : MainPage
+                      }
+                    />
+                    <PrivateRoute
+                      path="/office/:fileId"
+                      component={OfficeFilePage}
+                    />
+                    <RedirectablePublicRoute
+                      exact
+                      path="/register"
+                      component={<SignUpPage />}
+                    />
+                    <RedirectablePublicRoute
+                      exact
+                      path="/registerForm"
+                      component={<SignUpFormPage />}
+                    />
+                    <RedirectablePublicRoute
+                      exact
+                      path="/registerComplete"
+                      component={<SignUpCompletePage />}
+                    />
+                    <PrivateRoute
+                      path="/:resourceType(s|f|m)/:resourceId/:mainApp?"
+                      component={MainPage}
+                    />
+                    <Route path="/admin">
+                      <AdminPage />
+                    </Route>
+                    <Route component={NotFoundPage} />
+                  </Switch>
+                  {/* <PrivateRoute
             path="/users"
             component={MainPage}
           /> */}
-          </Switch>
-        </BrowserRouter>
-        {/* </I18nextProvider> */}
-      </PortalProvider>
+                </BrowserRouter>
+                {/* </I18nextProvider> */}
+              </PortalProvider>
+            </ReactKeycloakProvider>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </DndProvider>
   );
 }
