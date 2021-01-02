6d25d0e55eec667280ecd80582731e59514cfce9
DongHee Yang <donghee_yang@tmax.co.kr>
2021-01-03 01:42:10 +0900

    fix: 친구 추가 시 구성원이 있는 경우에도 친구 없음 메시지가 나오는 문제 수정
    -  조직이 있는 경우, 구성원 목록을 새로 가져오지 않아서 발생한 문제

--- src/utils/FriendsUtil.js
+++ src/utils/FriendsUtil.js
@@ -11,20 +11,22 @@ export const handleFriendsDialogType = async (
   const response = await orgStore.getOrgTree(params);
   if (response && response.length) {
     orgFunction();
-  } else {
-    try {
-      const useDomainKey =
-        process.env.REACT_APP_ENV === 'local' ? domainKey : undefined;
-      const res = await orgStore.getUserOrgUserList(
-        myProfile?.companyCode,
-        myProfile?.departmentCode,
-        myProfile?.id,
-        useDomainKey,
-      );
-      noOrgFunction(res);
-    } catch (e) {
-      console.log('getUserList Error');
-    }
+  }
+
+  // NOTE. 멤버 목록 조회는 조직이 있건 없건 필요함.
+  //  단 이후 페이지네이션을 적용하는 부분은 따로 고려해야 함.
+  try {
+    const useDomainKey =
+      process.env.REACT_APP_ENV === 'local' ? domainKey : undefined;
+    const res = await orgStore.getUserOrgUserList(
+      myProfile?.companyCode,
+      myProfile?.departmentCode,
+      myProfile?.id,
+      useDomainKey,
+    );
+    noOrgFunction(res);
+  } catch (e) {
+    console.log('getUserList Error');
   }
 };
 
