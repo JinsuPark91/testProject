d3fd9e51cc9edd836f59e69d3a664538e3dec01e
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-01-13 16:37:07 +0900

    feat: floating button 구현

--- src/components/common/FloatingButton.js
+++ src/components/common/FloatingButton.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  FloatingMenu,
+  MainButton,
+  ChildButton,
+} from 'react-floating-button-menu';
 import styled from 'styled-components';
-import Slider from 'react-slick';
 import Draggable from 'react-draggable';
 import { Tooltip } from 'antd';
-import { ChattingIcon, CancelIcon } from '../Icons';
 
-const ChatItem = ({ roomInfo, onItemClick, onItemClose }) => {
+import { ChattingIcon, CancelIcon, ArrowDownIcon, ArrowUpIcon } from '../Icons';
+
+const remToPixel = rem => {
+  return (
+    parseFloat(getComputedStyle(document.documentElement).fontSize, 10) * rem
+  );
+};
+
+const ChildItem = ({ roomInfo, onItemClick, onItemClose }) => {
   const handleClick = () => {
     onItemClick(roomInfo);
   };
@@ -16,42 +27,66 @@ const ChatItem = ({ roomInfo, onItemClick, onItemClose }) => {
   };
 
   return (
-    <ItemWrapper onClick={handleClick}>
+    <ChildItemWrapper onClick={handleClick}>
       <Tooltip placement="top" title={roomInfo.name} color="#0B1D41">
         <div>{roomInfo.name.slice(0, 4)}</div>
       </Tooltip>
       <div style={{ height: '1rem' }}>
         {roomInfo.userCount > 3 ? roomInfo.userCount : ''}
       </div>
-      <div className="close__button" onClick={handleClose}>
+      <div className="close-button" onClick={handleClose}>
         <CancelIcon width={0.8} height={0.8} color="#ffffff" />
       </div>
-    </ItemWrapper>
+    </ChildItemWrapper>
   );
 };
 
-const CustomArrow = ({ className, style, onClick }) => {
+const MainItem = ({
+  visible = false,
+  handleMainClick = () => {},
+  // visibleCloseButton = false,
+  // handleAllClose = () => {},
+}) => {
   return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'gray' }}
-      onClick={onClick}
-    />
+    <MainItemWrapper visible={visible} onClick={handleMainClick}>
+      <ChattingIcon width={2.69} height={1.88} />
+      {/* {visibleCloseButton ? (
+        <div className="close-button" onClick={handleAllClose}>
+          <CancelIcon width={0.8} height={0.8} color="#ffffff" />
+        </div>
+      ) : null} */}
+    </MainItemWrapper>
   );
+};
+
+const ArrowIcon = ({ width, height, isPrev }) => {
+  const props = {
+    width,
+    height,
+    color: 'gray',
+  };
+  return isPrev ? <ArrowDownIcon {...props} /> : <ArrowUpIcon {...props} />;
 };
 
 const FloatingButton = ({
   visible = false,
-  roomList = [],
-  slidesToShow = 7,
+  rooms = [],
+  count = 5,
   onCloseAll = () => {},
   onItemClick = () => {},
   onItemClose = () => {},
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isDrag, setIsDrag] = useState(false);
+  const [startIndex, setStartIndex] = useState(0);
 
-  const handleToggle = () => {
+  useEffect(() => {
+    if (isOpen) {
+      setStartIndex(0);
+    }
+  }, [isOpen]);
+
+  const handleMainClick = () => {
     if (!isDrag) {
       setIsOpen(!isOpen);
     }
@@ -63,74 +98,158 @@ const FloatingButton = ({
   };
 
   const handleAllClose = e => {
-    e.stopPropagation();
-    if (!isDrag) {
-      onCloseAll();
-    }
-    setIsDrag(false);
+    onCloseAll();
   };
 
-  const settings = {
-    dots: false,
-    infinite: false,
-    slidesToShow,
-    slidesToScroll: 1,
-    vertical: true,
-    draggable: false,
-    speed: 100,
-    nextArrow: <CustomArrow />,
-    prevArrow: <CustomArrow />,
+  const mainItem = (
+    <MainItem
+      rooms={rooms}
+      visible={visible}
+      visibleCloseButton={!!rooms.length}
+      // handleAllClose={handleAllClose}
+    />
+  );
+
+  const next = () => {
+    if (rooms.length - count > startIndex) {
+      setStartIndex(startIndex + 1);
+    }
   };
+
+  const prev = () => {
+    if (startIndex > 0) {
+      setStartIndex(startIndex - 1);
+    }
+  };
+
+  if (!visible) return null;
 
   return (
     <Draggable bounds="parent" onDrag={handleDrag}>
       <Wrapper>
         {isOpen && (
-          <CustomSlider {...settings} style={{ width: '4.38rem' }}>
-            {roomList.map(roomInfo => (
-              <ChatItem
-                key={roomInfo.id}
-                roomInfo={roomInfo}
-                onItemClick={onItemClick}
-                onItemClose={onItemClose}
-              />
-            ))}
-          </CustomSlider>
+          <AllCloseButton onClick={handleAllClose}>모두 종료</AllCloseButton>
         )}
+        <CustomFloatingMenu
+          spacing={remToPixel(0.63)}
+          direction="up"
+          slideSpeed={300}
+          isOpen={isOpen}
+          size={remToPixel(2)}
+          isVisible={isOpen}
+        >
+          <CustomMainButton
+            iconResting={mainItem}
+            iconActive={mainItem}
+            size={remToPixel(3.5)}
+            onClick={handleMainClick}
+          />
+          <ArrowChildButton
+            icon={
+              rooms.length > count && startIndex !== 0 ? (
+                <ArrowIcon width={1.25} height={1.25} isPrev />
+              ) : null
+            }
+            onClick={rooms.length > count && startIndex !== 0 ? prev : null}
+            size={remToPixel(1.5)}
+            visible={rooms.length > count && startIndex !== 0}
+          />
 
-        <ToggleButton visible={visible} onClick={handleToggle}>
-          <ChattingIcon width={2.69} height={1.88} />
-          {roomList.length ? (
-            <div className="close__button" onClick={handleAllClose}>
-              <CancelIcon width={0.8} height={0.8} color="#ffffff" />
-            </div>
-          ) : null}
-        </ToggleButton>
+          {rooms.slice(startIndex, count + startIndex).map(roomInfo => (
+            <ChildButton
+              icon={
+                <ChildItem
+                  roomInfo={roomInfo}
+                  onItemClick={onItemClick}
+                  onItemClose={onItemClose}
+                />
+              }
+              size={remToPixel(3.5)}
+            />
+          ))}
+
+          <ArrowChildButton
+            icon={
+              rooms.length > count && startIndex < rooms.length - count ? (
+                <ArrowIcon width={1.25} height={1.25} />
+              ) : null
+            }
+            onClick={
+              rooms.length > count && startIndex < rooms.length - count
+                ? next
+                : null
+            }
+            size={remToPixel(1.5)}
+            visible={rooms.length > count && startIndex < rooms.length - count}
+          />
+        </CustomFloatingMenu>
       </Wrapper>
     </Draggable>
   );
 };
 
-const CustomSlider = styled(Slider)`
-  & .slick-slide {
-    display: flex;
-    justify-content: center;
+export default FloatingButton;
+
+const CustomFloatingMenu = styled(FloatingMenu)`
+  & li {
+    display: ${({ isVisible }) => (isVisible ? 'flex' : 'none')};
   }
 `;
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   position: fixed;
-  right: 30px;
-  bottom: 30px;
-  padding: 0 0.5rem;
-  z-index: 5;
+  right: 1rem;
+  bottom: 1rem;
+`;
+
+const AllCloseButton = styled.div`
+  display: flex;
+  width: 4.75rem;
+  height: 1.88rem;
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  border: 1px solid #c6ced6;
+  border-radius: 0.9rem;
+  font-size: 0.75rem;
+  color: #3b3b3b;
+  align-self: flex-end;
+  margin-right: 0.84rem;
+  position: absolute;
+  right: 3.5rem;
+  box-shadow: 0 0 5px 0 #c6ced6;
+
+  &:hover {
+    background: #dcddff;
+  }
+
+  &:active {
+    background: #ffffff;
+    box-shadow: unset;
+    border: 1px solid #6c56e5;
+  }
+`;
+
+const CustomMainButton = styled(MainButton)`
+  & > div {
+    transform: unset;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ArrowChildButton = styled(ChildButton)`
+  cursor: ${({ visible }) => (visible ? 'pointer' : 'default')};
+  margin-top: -10px;
+  margin-bottom: 0;
+  box-shadow: unset;
 `;
 
 const RoundButton = styled.div`
   display: flex;
+  width: 100%;
+  height: 100%;
   align-items: center;
   justify-content: center;
   flex-direction: column;
@@ -141,7 +260,12 @@ const RoundButton = styled.div`
   font-size: 0.69rem;
   position: relative;
 
-  & .close__button {
+  & .close-button {
+    width: 1.88rem;
+    height: 1.88rem;
+  }
+
+  & .close-button {
     display: none;
     align-items: center;
     justify-content: center;
@@ -150,48 +274,35 @@ const RoundButton = styled.div`
     bottom: 0;
   }
 
-  &:hover .close__button {
+  &:hover .close-button {
     display: flex;
   }
 `;
 
-const ToggleButton = styled(RoundButton)`
-  width: 4.38rem;
-  height: 4.38rem;
+const MainItemWrapper = styled(RoundButton)`
   background: #0b1d41;
 
   &:hover {
     background: #151559;
   }
 
-  & .close__button {
-    width: 2.13rem;
-    height: 2.13rem;
+  &:active {
+    background: #342f7d;
   }
 
-  & .close__button:hover {
-    background: rgba(11, 29, 65, 0.8);
-  }
+  // & .close-button:hover {
+  //   background: rgba(0, 0, 0, 0.4);
+  // }
 `;
 
-const ItemWrapper = styled(RoundButton)`
-  width: 3.75rem;
-  height: 3.75rem;
+const ChildItemWrapper = styled(RoundButton)`
   background: #6c56e5;
-  margin: 0.32rem 0;
-
-  & .close__button {
-    width: 1.88rem;
-    height: 1.88rem;
-  }
 
   &:hover {
     background: rgba(86, 68, 183, 1);
   }
 
-  & .close__button:hover {
+  & .close-button:hover {
     background: rgba(0, 0, 0, 0.4);
   }
 `;
-
-export default FloatingButton;
