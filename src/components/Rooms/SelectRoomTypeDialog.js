c0201369ca0b1867fd0d11cdbef44e1915dfabd8
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-01-19 11:04:52 +0900

    fix: 룸 생성 dialog 디자인 수정

--- src/components/Rooms/SelectRoomTypeDialog.js
+++ src/components/Rooms/SelectRoomTypeDialog.js
@@ -4,32 +4,34 @@ import styled from 'styled-components';
 import { Typography, Button, Modal } from 'antd';
 import { useCoreStores } from 'teespace-core';
 import { talkRoomStore } from 'teespace-talk-app';
-import privateRoomImg from '../../assets/private_room.svg';
-import openRoomImg from '../../assets/open_room.svg';
+import { PrivateRoomIcon, OpenChatIcon } from '../Icons';
 import CreatePrivateRoomDialog from '../dialogs/CreatePrivateRoomDialog';
 import CreatePublicRoomDialog from '../dialogs/CreatePublicRoomDialog';
-import { getQueryParams, getQueryString } from '../../utils/UrlUtil';
 
 const { Title } = Typography;
 
+const VerticalBar = styled.div`
+  height: 10rem;
+  width: 1px;
+  background: #ddd9d4;
+  margin: 0 1.53rem;
+`;
+
 const SelectRoomType = styled.div`
   width: 100%;
-  padding: 2.25rem 2rem;
+  padding: 3.13rem 2.19rem 2.5rem 2.19rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
 const RoomInformation = styled.div`
-  width: 100%;
-  max-width: 50%;
+  width: 50%;
   display: flex;
   flex-flow: column wrap;
   align-items: center;
-  &:last-of-type {
-    margin-left: 3.75rem;
-  }
 `;
+
 const StyledInfoTitle = styled(Title)`
   font-size: 0.94rem;
   line-height: 1.38rem;
@@ -38,18 +40,15 @@ const StyledInfoTitle = styled(Title)`
   margin-bottom: 0.63rem;
 `;
 const StyledInfoText = styled.p`
-  margin-bottom: 0.13rem;
+  margin-bottom: 2.88rem;
   font-size: 0.75rem;
-  word-break: break-all;
   color: #696969;
+  white-space: pre-line;
   letter-spacing: 0;
   text-align: center;
   line-height: 1rem;
 `;
-const StyledInfoImg = styled.img`
-  width: 6.88rem;
-  margin-bottom: 0.63rem;
-`;
+
 const StyledButton = styled(Button)`
   &.ant-btn {
     width: 8.38rem;
@@ -186,12 +185,15 @@ function SelectRoomTypeDialog({ visible, onCancel, onCreateRoom = () => {} }) {
       >
         <SelectRoomType>
           <RoomInformation>
+            <div style={{ marginBottom: '1.19rem' }}>
+              <PrivateRoomIcon width={1.88} height={1.88} color="#232D3B" />
+            </div>
             <StyledInfoTitle level={4}>프라이빗 룸</StyledInfoTitle>
             <StyledInfoText>
-              프라이빗 룸을 통해 간단한 대화를 나누어 보세요. 구성원들만의
-              개인적인 공간입니다.
+              {`프라이빗 룸을 통해 간단한 대화를 
+              나누어 보세요. 구성원들만의
+              개인적인 공간입니다.`}
             </StyledInfoText>
-            <StyledInfoImg src={privateRoomImg} alt="" />
             <StyledButton
               type="solid"
               shape="round"
@@ -200,13 +202,17 @@ function SelectRoomTypeDialog({ visible, onCancel, onCreateRoom = () => {} }) {
               프라이빗 룸 만들기
             </StyledButton>
           </RoomInformation>
+          <VerticalBar />
           <RoomInformation>
+            <div style={{ marginBottom: '1.19rem' }}>
+              <OpenChatIcon width={1.88} height={1.88} color="#232D3B" />
+            </div>
             <Title level={4}>오픈 룸</Title>
             <StyledInfoText>
-              오픈 룸을 통해 특정 주제, 프로젝트를 진행해보세요. 누구나 검색을
-              통하여 자유롭게 참여할 수 있는 공간입니다.
+              {`오픈 룸을 통해 특정 주제, 프로젝트를 
+              진행해보세요. 누구나 검색을 통하여
+              자유롭게 참여할 수 있는 공간입니다.`}
             </StyledInfoText>
-            <StyledInfoImg src={openRoomImg} alt="" />
             <StyledButton
               type="solid"
               shape="round"
