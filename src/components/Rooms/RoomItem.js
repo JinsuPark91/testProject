fa06414a9584000d3c74467a614179efc846bdc3
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-01-28 13:57:02 +0900

    fix: room item alarm 임시 숨김

--- src/components/Rooms/RoomItem.js
+++ src/components/Rooms/RoomItem.js
@@ -162,7 +162,7 @@ const RoomDropdown = React.memo(
               룸 상단 고정
             </Menu.Item>
           )}
-          {roomInfo.isAlarmUsed ? (
+          {/* {roomInfo.isAlarmUsed ? (
             <Menu.Item key="disableAlarm" onClick={handleAlarmDisable}>
               알림 끄기
             </Menu.Item>
@@ -170,7 +170,7 @@ const RoomDropdown = React.memo(
             <Menu.Item key="enableAlarm" onClick={handleAlarmEnable}>
               알림 켜기
             </Menu.Item>
-          )}
+          )} */}
           <Menu.Item key="member" onClick={handleViewMember}>
             룸 구성원 보기
           </Menu.Item>
