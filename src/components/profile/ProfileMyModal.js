9add82f80f64c2079362af8875c82304dae79410
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-02-02 16:24:48 +0900

    fix: tmax domain 판별식 수정

--- src/components/profile/ProfileMyModal.js
+++ src/components/profile/ProfileMyModal.js
@@ -211,10 +211,12 @@ const ProfileMyModal = ({
     setIsCreated(created);
   }, [created]);
 
+  const isTmaxDomain = !!/^(tmax)\./gi.exec(window.location.hostname);
+
   // 이후 '현재 스페이스의 어드민'인지를 체크하도록 수정
   const moreMenu = (
     <Menu style={{ minWidth: '6.25rem' }}>
-      {window.location.hostname !== 'tmax.wapl.ai' ? (
+      {!isTmaxDomain ? (
         <Menu.Item onClick={handleInviteDialog}>구성원 초대</Menu.Item>
       ) : null}
       <Menu.Item onClick={handleMemberList}>구성원 목록</Menu.Item>
