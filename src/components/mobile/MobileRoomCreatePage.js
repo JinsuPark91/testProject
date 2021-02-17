b416945e9dd39296c221ff13bb4c0fd39d54fa00
박창환 <changhwan_park@tmax.co.kr>
2021-02-17 14:11:52 +0900

    feat(com): core - chungnam 연동

--- src/components/mobile/MobileRoomCreatePage.js
+++ src/components/mobile/MobileRoomCreatePage.js
@@ -1,31 +1,14 @@
 import React, { useState, useCallback } from 'react';
-import { ItemSelector, useCoreStores } from 'teespace-core';
+import { MobileItemSelector, useCoreStores } from 'teespace-core';
 
-const MobileRoomCreatePage = ({ onOk, onCancel }) => {
+const MobileRoomCreatePage = ({ onCancel }) => {
   const { userStore } = useCoreStores();
-  const initialOptions = {
-    isChangeName: false,
-    roomName: '',
-    isStartMeeting: false,
-  };
-  const [options, setOptions] = useState(initialOptions);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const disabledIds = [userStore.myProfile.id];
 
-  const clearState = () => {
-    setOptions(initialOptions);
-  };
-
-  const handleOk = () => {
-    onOk({
-      ...options,
-      selectedUsers,
-    });
-  };
-
-  const handleCancel = () => {
+  const handleCreateRoom = () => {
+    console.log(`selected user is${selectedUsers}`);
     onCancel();
-    clearState();
   };
 
   const handleSelectedUserChange = useCallback(({ userArray }) => {
@@ -33,6 +16,7 @@ const MobileRoomCreatePage = ({ onOk, onCancel }) => {
       user => !disabledIds.includes(user.friendId || user.id),
     );
     setSelectedUsers(filteredUsers);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -40,10 +24,9 @@ const MobileRoomCreatePage = ({ onOk, onCancel }) => {
       <div>
         <span onClick={onCancel}>뒤로가기</span>
         <span>프라이빗 룸 만들기</span>
-        <span>초대 N</span>
+        <span onClick={handleCreateRoom}>초대 N</span>
       </div>
-      <ItemSelector
-        showTagList={false}
+      <MobileItemSelector
         isVisibleRoom={false}
         onSelectChange={handleSelectedUserChange}
         disabledIds={disabledIds}
@@ -51,18 +34,6 @@ const MobileRoomCreatePage = ({ onOk, onCancel }) => {
         showMeOnFriendTab={false}
         height={25} // rem
       />
-      {/* <ConfigWrapper>
-        <ConfigTitle>
-          <Checkbox
-            className="check-round"
-            defaultChecked
-            disabled={selectedUsers.length < 2}
-            checked={options.isChangeName}
-            onChange={handleChangeNameChange}
-          />
-          <ConfigTitleText>룸 이름 설정하기</ConfigTitleText>
-        </ConfigTitle>
-      </ConfigWrapper> */}
     </>
   );
 };
