8b5f576217012bdbf9bdf2aeab76697b395abd88
박창환 <changhwan_park@tmax.co.kr>
2021-02-22 16:17:50 +0900

    refactor(com): friends lnb style 분리

---
+++ src/styles/friend/FriendsLnbStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+
+// eslint-disable-next-line import/prefer-default-export
+export const FriendsLnbWrapper = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #ffffff;
+`;
