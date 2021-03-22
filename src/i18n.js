ce7467441945e7a330b2e8f59138f5e859eebd93
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-03-22 15:27:09 +0900

    feat: i18n language detector 추가 및 order 설정

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,25 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const i18n = i18next.createInstance();
 i18n
+  .use(LanguageDetector)
   .use(Backend)
   .use(initReactI18next)
   .init({
-    debug: false,
-    lng: 'ko',
-    fallbackLng: 'en',
+    debug: true,
     ns: ['translation'],
     defaultNS: 'translation',
     keySeparator: false,
     interpolation: { escapeValue: false },
     backend: {
       loadPath: `/locales/{{lng}}/{{ns}}.json`,
+    },
+    detection: {
+      order: ['sessionStorage', 'navigator'],
+      lookupSessionStorage: 'language',
     },
     react: {
       useSuspense: false,
