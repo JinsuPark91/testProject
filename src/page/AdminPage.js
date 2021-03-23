292ab5eba58574e0767d454d8fb9e746460c90f9
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-03-23 15:32:52 +0900

    fix: admin component에 language prop 전달

--- src/page/AdminPage.js
+++ src/page/AdminPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { AdminPage as AdminPageComponent } from 'teespace-admin';
+import { useTranslation } from 'react-i18next';
 
 function AdminPage() {
-  return <AdminPageComponent />;
+  const { i18n } = useTranslation();
+  return <AdminPageComponent language={i18n.language} />;
 }
 
 export default AdminPage;
