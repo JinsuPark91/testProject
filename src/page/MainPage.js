e8cdf1fd67a4684e9a97647092387777aede46a7
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-04-13 09:43:49 +0900

    feat: mini talk header 사진 클릭시 기존이랑 동일 동작하도록 수정

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -11,7 +11,6 @@ import {
   Toast,
   Message,
 } from 'teespace-core';
-import { Observer } from 'mobx-react';
 import { beforeRoute as noteBeforeRoute } from 'teespace-note-app';
 import { WindowMail } from 'teespace-mail-app';
 import { Prompt } from 'react-router';
@@ -22,9 +21,8 @@ import { Loader, Wrapper } from './MainPageStyle';
 import PlatformUIStore from '../stores/PlatformUIStore';
 import LoadingImg from '../assets/WAPL_Loading.gif';
 import FaviconChanger from '../components/common/FaviconChanger';
-import FloatingButton from '../components/common/FloatingButton';
+import WindowManager from '../components/common/WindowManager';
 import { getQueryParams, getQueryString } from '../utils/UrlUtil';
-import { runWatcher, stopWatcher } from '../utils/Watcher';
 
 const MainPage = () => {
   const { t } = useTranslation();
@@ -383,78 +381,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-// [TODO] : 나중에 다른데로 옮기자.
-const Window = ({ windowInfo }) => {
-  const { id: windowId, type: app } = windowInfo;
-  const url = `/s/${windowId}/${app}?mini=true`;
-
-  const getSpecs = appName => {
-    if (appName === 'meeting') return '';
-
-    const width = 600;
-    const height = 800;
-    const options = {
-      width,
-      height,
-      top: (window.innerHeight - height) / 2 + window.screenY,
-      left: (window.innerWidth - width) / 2 + window.screenX,
-    };
-    return Object.entries(options)
-      .map(entry => entry.join('='))
-      .join(',');
-  };
-
-  useEffect(() => {
-    const handler = window.open(url, '_blank', getSpecs(app));
-    const info = PlatformUIStore.getWindow(app, windowId);
-    if (info) {
-      info.handler = handler;
-      handler.focus();
-    }
-  }, []);
-
-  return null;
-};
-
-const WindowManager = () => {
-  useEffect(() => {
-    runWatcher();
-
-    return () => stopWatcher();
-  }, []);
-
-  return (
-    <Observer>
-      {() => {
-        const talkWindows = PlatformUIStore.getWindows('talk');
-        return (
-          <>
-            {PlatformUIStore.getWindows('talk').map(window => (
-              <Window key={window.id} windowInfo={window} />
-            ))}
-            {PlatformUIStore.getWindows('meeting').map(window => (
-              <Window key={window.id} windowInfo={window} />
-            ))}
-            <FloatingButton
-              // visible={talkWindows.length}
-              visible={false}
-              rooms={talkWindows}
-              count={5}
-              onItemClick={roomInfo => {
-                PlatformUIStore.focusWindow('talk', roomInfo.id);
-              }}
-              onItemClose={roomInfo => {
-                console.log('ROOM INFO : ', roomInfo);
-                PlatformUIStore.closeWindow('talk', roomInfo.id);
-              }}
-              onCloseAll={() => {
-                PlatformUIStore.closeAllWindow('talk');
-              }}
-            />
-          </>
-        );
-      }}
-    </Observer>
-  );
-};
