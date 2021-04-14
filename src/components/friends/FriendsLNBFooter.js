f51a50720dca7d511c858596644dabdda75d6603
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-04-14 11:53:55 +0900

    fix: config에 따른 friend list logo 처리

--- src/components/friends/FriendsLNBFooter.js
+++ src/components/friends/FriendsLNBFooter.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useCoreStores } from 'teespace-core';
 import { WaplLogo } from '../Icons';
 import { FooterWrapper } from '../../styles/friends/FriendsLNBStyle';
 
 const FriendsLNBFooter = ({ shadow }) => {
+  const { configStore } = useCoreStores();
+
+  if (!configStore.isActivateComponent('Platform', 'LNB:Logo')) return null;
+
   return (
     <FooterWrapper shadow={shadow ? 1 : 0}>
       <WaplLogo />
