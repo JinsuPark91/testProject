50b7ad1b3f06f356cc3c5725df25ff2d0c4b0fd2
bin_lim <bin_lim@tmax.co.kr>
2021-04-15 22:07:49 +0900

    fix: cnu 라우팅 수정

--- src/libs/KsignRedirectRoute.js
+++ src/libs/KsignRedirectRoute.js
@@ -18,17 +18,6 @@ export default function KsignRedirectRoute({ component: Component, ...rest }) {
     }
   }, [authStore.user.id, authStore.isAuthenticated]);
 
-  // useEffect(() => {
-  //   const preLogout = async () => {
-  //     await authStore.logout();
-  //     // Cookies.remove('ACCESS_TOKEN');
-  //     // Cookies.remove('DEVICE_TYPE');
-  //   };
-  //   if (authStore.user?.loginId) {
-  //     preLogout();
-  //   }
-  // }, [authStore]);
-
   let loginInfo;
   if (window.location.pathname.includes('/mobile')) {
     loginInfo = {
@@ -52,78 +41,6 @@ export default function KsignRedirectRoute({ component: Component, ...rest }) {
     };
   }
 
-  /*
-
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        //로그인 된 경우
-        if (authStore.isAuthenticated) {
-          return <Component />;
-        }
-        //가장 처음 loginId & deviceId 있을때 index.jsp호출한다.
-        else if (getLoginId) {
-          const stateFrom = props.location.state?.from;
-
-          Cookies.set(
-            'routingPath',
-            stateFrom?.pathname ? stateFrom.pathname : window.location.pathname,
-            process.env.REACT_APP_ENV === 'local'
-              ? {}
-              : {
-                  domain: `.${window.location.hostname}`,
-                },
-          );
-          // history.push(`/cnu/sso/index2.jsp?deviceId=${getDeviceId}`)
-          if (process.env.REACT_APP_ENV === 'local') {
-            window.location.href = `https://test-id44.teespace.com/cnu/sso/index.jsp?deviceId=${getDeviceId}`;
-          } else {
-            window.location.href = `/cnu/sso/index.jsp?deviceId=${getDeviceId}`;
-          }
-        } else if (!getLoginId) {
-          console.log(loginInfo);
-          const getRoutingPath = Cookies.get('routingPath');
-          console.log(`getRoturingPath`, getRoutingPath);
-          // Promise.all([authStore.login(loginInfo)])
-          // .then(async res => {
-          // if (res) {
-          // history.replace(window.location.pathname);
-          // NOTE. 이전 경로가 존재하면 해당 경로로 이동
-          const stateFrom = props.location.state?.from;
-          console.log('stateFrom', stateFrom);
-          if (stateFrom) {
-            history.push(
-              `${stateFrom.pathname}${props.location.state?.from.search}`,
-            );
-          } else {
-            if (window.location.pathname.includes('/mobile')) {
-              const exceptMobilePath = getRoutingPath.replace('/mobile', '');
-              if (exceptMobilePath.includes('login')) {
-                console.log(
-                  '###########################################################',
-                );
-                history.push(`/friend`);
-              } else {
-                history.push(exceptMobilePath);
-              }
-            } else {
-              history.push(`/f/${authStore.user.id}/profile`);
-            }
-          }
-          // }
-          return null;
-          // })
-          // .catch(e => {
-          //   history.push('/privatelogin');
-          // });
-        }
-      }}
-    />
-  );
-
-  */
-
   return (
     <Route
       {...rest}
@@ -131,52 +48,44 @@ export default function KsignRedirectRoute({ component: Component, ...rest }) {
         (async () => {
           try {
             const stateFrom = props.location.state?.from;
+            const getRoutingPath = Cookies.get('routingPath') ? Cookies.get('routingPath')  : "/mobile/login"
+            const getNibId = Cookies.get('NIBID');
 
-            if (getLoginId) {
-              Cookies.set(
-                'routingPath',
-                stateFrom?.pathname
-                  ? stateFrom.pathname
-                  : window.location.pathname,
-                process.env.REACT_APP_ENV === 'local'
-                  ? {}
-                  : {
-                      domain: `.${window.location.hostname}`,
-                    },
-              );
+            const res = await authStore.login(loginInfo);
 
-              if (process.env.REACT_APP_ENV === 'local') {
-                window.location.href = `https://test-id44.teespace.com/cnu/sso/index.jsp?deviceId=${getDeviceId}`;
-              } else {
-                window.location.href = `/cnu/sso/index.jsp?deviceId=${getDeviceId}`;
-              }
-            } else {
-              const res = await authStore.login(loginInfo);
-              const getRoutingPath = Cookies.get('routingPath');
-              const getNibId = Cookies.get('NIBID');
-              if (res) {
-                if (getNibId) {
-                  if (stateFrom) {
-                    history.push(
-                      `${stateFrom.pathname}${props.location.state?.from.search}`,
-                    );
-                  }
-                } else if (window.location.pathname.includes('/mobile')) {
-                  const exceptMobilePath = getRoutingPath?.replace(
-                    '/mobile',
-                    '',
+            if (res) {
+              // if (getNibId) {
+                  // NOTE. 이전 경로가 존재하면 해당 경로로 이동
+                if (stateFrom) {
+                  history.push(
+                    `${stateFrom.pathname}${props.location.state?.from.search}`,
                   );
-                  if (exceptMobilePath.includes('login')) {
-                    history.push(`/friend`);
-                  } else {
-                    history.push(exceptMobilePath);
-                  }
                 } else {
-                  history.push(`/f/${authStore.user.id}/profile`);
+                  if (getRoutingPath.includes('/mobile')) {
+                    const exceptMobilePath = getRoutingPath.replace(
+                      '/mobile',
+                      '',
+                    );
+                    if (exceptMobilePath.includes('/login')) {
+                      history.push(`/friend`);
+                    } else {
+                      history.push(exceptMobilePath);
+                    }
+                  } else {
+                    history.push(`/f/${authStore.user.id}/profile`);
+                  }
                 }
-              }
+              // } else if (getRoutingPath.includes('/mobile')) {
+              //   const exceptMobilePath = getRoutingPath.replace('/mobile', '');
+              //   if (exceptMobilePath.includes('login')) {
+              //     history.push(`/friend`);
+              //   } else {
+              //     history.push(exceptMobilePath);
+              //   }
+              // } else {
+              //   history.push(`/f/${authStore.user.id}/profile`);
+              // }
             }
-            // NOTE. 이전 경로가 존재하면 해당 경로로 이동
           } catch (e) {
             history.push('/privatelogin');
             console.error(e);
