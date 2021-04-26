a17822c3d733c5ae2ec097d46dd6e91c5c03ebd6
박창환 <changhwan_park@tmax.co.kr>
2021-04-26 17:54:02 +0900

    refactor: 상단 프로필 modal style 파일 분리 및 코드 개선 작업 중

--- src/components/profile/ProfileMyModal.js
+++ src/components/profile/ProfileMyModal.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import styled, { css } from 'styled-components';
-import { Button, Avatar, Dropdown, Menu, Checkbox, Tooltip } from 'antd';
+import { Button, Dropdown, Menu, Checkbox, Tooltip } from 'antd';
 import {
   useCoreStores,
   Toast,
@@ -16,18 +15,49 @@ import { useHistory } from 'react-router-dom';
 import { useObserver, Observer } from 'mobx-react';
 import { useTranslation } from 'react-i18next';
 import { fallbackLanguage } from '../../i18n';
-import PlatformUIStore from '../../stores/PlatformUIStore';
 import ProfileSpaceModal from './ProfileSpaceModal';
 import SelectRoomTypeDialog from '../Rooms/SelectRoomTypeDialog';
 import SpaceEditModal from './SpaceEditModal';
 import MovePage from '../../utils/MovePage';
-import { SELECTED_TAB } from '../usersettings/SettingConstants';
 import { getMainWaplURL } from '../../utils/UrlUtil';
-import { handleFriendsDialogType } from '../../utils/FriendsUtil';
 import { isSpaceAdmin } from '../../utils/GeneralUtil';
 import { ArrowRightIcon } from '../Icons';
-import convertSpaceIcon from '../../assets/convert_space.svg';
-import moreSpaceIcon from '../../assets/view_more.svg';
+import {
+  UserImage,
+  UserName,
+  UserMail,
+  UserStatus,
+  UserButtonBox,
+  UserBar,
+  LogoutButton,
+  UserSpaceArea,
+  DataName,
+  DataBox,
+  NewBadge,
+  Logo,
+  Info,
+  Title,
+  Blind,
+  UserSubArea,
+  LanguageButton,
+  ConvertDropdown,
+  ConvertNow,
+  LogoSmall,
+  LogoNumber,
+  NowInfo,
+  NowTitle,
+  ConvertList,
+  ConvertItem,
+  ItemText,
+  ConvertBox,
+  ConvertAdd,
+  AddBox,
+  AddText,
+  ConvertMove,
+  UserSettingArea,
+  SettingButton,
+  SettingBar,
+} from '../../styles/profile/ProfileMyModalStyle';
 import { ReactComponent as SquareSpaceIcon } from '../../assets/card_view.svg';
 import LanguageIcon from '../../assets/language.svg';
 
@@ -40,25 +70,21 @@ const ProfileMyModal = ({
 }) => {
   const { t, i18n } = useTranslation();
   const { userStore, spaceStore, configStore } = useCoreStores();
-  const { isGuest } = userStore.myProfile;
   const history = useHistory();
+  const profile = userStore.myProfile;
+  const { isGuest } = userStore.myProfile;
+
   const [isCreated, setIsCreated] = useState(created);
-  const [profile, setProfile] = useState(null);
   const [settingDialogVisible, setSettingDialogVisible] = useState(false);
   const [spaceListVisible, setSpaceListVisible] = useState(false);
-  const [moreMenuDropDownVisible, setMoreMenuDropDownVisible] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
 
   const [isInviteDialogOpen, setIsInviteDialogOpen] = useState(false);
   const [isFriendMemViewOpen, setIsFriendMemViewOpen] = useState(false);
 
-  const [spaceMemberList, setSpaceMemberList] = useState([]);
   const [isToastOpen, setIsToastOpen] = useState(false);
   const [toastText, setToastText] = useState('');
   const [isViewMode, setIsViewMode] = useState(true);
-
-  const [isOrgExist, setIsOrgExist] = useState(false);
-  const [modalTitle, setModalTitle] = useState('');
 
   const [isRoomDialogVisible, setIsRoomDialogVisible] = useState(false);
   const [isSpaceEditDialogVisible, setIsSpaceEditDialogVisible] = useState(
@@ -69,8 +95,7 @@ const ProfileMyModal = ({
     setIsNewSpaceErrorMessageVisible,
   ] = useState(false);
 
-  // eslint-disable-next-line no-unused-vars
-  const handleSettingDialogOpen = useCallback(e => {
+  const handleSettingDialogOpen = useCallback(() => {
     setIsCreated(false);
     setSettingDialogVisible(true);
     setSpaceListVisible(false);
@@ -91,11 +116,8 @@ const ProfileMyModal = ({
     });
     setSpaceListVisible(prevVisible => !prevVisible);
     setIsCreated(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const handleMoreMenuDropDownVisible = () => {
-    setMoreMenuDropDownVisible(!moreMenuDropDownVisible);
-  };
 
   const handleLogout = async () => {
     // Close dialog first
@@ -109,7 +131,6 @@ const ProfileMyModal = ({
 
   const handleCancel = useCallback(() => {
     setSpaceListVisible(false);
-    setMoreMenuDropDownVisible(false);
     setTimeout(() => onCancel(), 1);
   }, [onCancel]);
 
@@ -118,60 +139,39 @@ const ProfileMyModal = ({
   }, []);
 
   const handleInviteDialog = useCallback(() => {
-    setMoreMenuDropDownVisible(false);
     setIsInviteDialogOpen(true);
     logEvent('threedot', 'clickInviteMemberBtn');
   }, []);
 
   const handleMemberList = useCallback(async () => {
-    setMoreMenuDropDownVisible(false);
-    try {
-      await handleFriendsDialogType(
-        () => setIsOrgExist(true),
-        res => setSpaceMemberList(res),
-      );
-      setIsViewMode(true);
-      setModalTitle(spaceStore.currentSpace?.name);
-      setIsFriendMemViewOpen(true);
-      logEvent('threedot', 'clickShowMemberList');
-    } catch (e) {
-      console.log('service Error...');
-    }
-  }, [spaceStore]);
+    setIsViewMode(true);
+    setIsFriendMemViewOpen(true);
+    logEvent('threedot', 'clickShowMemberList');
+  }, []);
 
   const handleAddFriend = useCallback(async () => {
-    await handleFriendsDialogType(
-      () => setIsOrgExist(true),
-      res => setSpaceMemberList(res),
-    );
     setIsViewMode(false);
-    setModalTitle(t('CM_ADD_PHOTO_FRIENDS'));
     setIsFriendMemViewOpen(true);
   }, []);
 
   const handleSpaceEditDialog = useCallback(() => {
-    setMoreMenuDropDownVisible(false);
     setIsSpaceEditDialogVisible(true);
   }, []);
 
   const handleAdminPage = useCallback(() => {
-    setMoreMenuDropDownVisible(false);
     window.open(`${window.location.origin}/admin`);
   }, []);
-
   const handleMoveSpacePage = useCallback(() => {
     MovePage('spaces');
   }, []);
-
   const handleMoveAccountPage = useCallback(() => {
     MovePage('account?open=password');
   }, []);
-
-  const handleOpenSupport = () => {
+  const handleOpenSupport = useCallback(() => {
     MovePage('support', true);
-  };
+  }, []);
 
-  const handleClickNewSpace = useCallback(() => {
+  const handleClickNewSpace = () => {
     try {
       const adminSpace = spaceStore.getAdminSpaces({
         loginId: userStore.myProfile?.loginId,
@@ -184,16 +184,7 @@ const ProfileMyModal = ({
     } catch (e) {
       console.log('SpaceStore Adminspace Get Error...');
     }
-  }, [spaceStore, userStore]);
-
-  useEffect(() => {
-    if (isEditMode === true) return;
-    (async () => {
-      const userProfile = await userStore.getProfile({ userId });
-      setProfile(userProfile);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isEditMode, userStore]);
+  };
 
   useEffect(() => {
     setIsCreated(created);
@@ -245,7 +236,6 @@ const ProfileMyModal = ({
     return fallbackLanguage;
   };
 
-  // NOTE: 메뉴는 언어 설정과 관계없음
   const LanguageMenu = (
     <Menu style={{ minWidth: '6.25rem' }}>
       <Menu.Item onClick={() => handleChangeLanguage('ko')}>
@@ -290,15 +280,6 @@ const ProfileMyModal = ({
       visible={isEditMode}
       onClose={toggleEditMode}
       profilePhoto={thumbPhoto}
-      onClickMeeting={roomId => {
-        PlatformUIStore.openWindow({
-          id: roomId,
-          type: 'meeting',
-          name: null,
-          userCount: null,
-          handler: null,
-        });
-      }}
       editMode
     />
   );
@@ -417,12 +398,12 @@ const ProfileMyModal = ({
                   ))}
               </ConvertList>
             )}
-            <ConvertAdd onClick={() => handleClickNewSpace()}>
+            <ConvertAdd onClick={handleClickNewSpace}>
               <AddBox>+</AddBox>
               <AddText addSpace>{t('CM_CREATE_CONTENTS_AREA_02')}</AddText>
             </ConvertAdd>
           </ConvertBox>
-          <ConvertMove onClick={() => handleMoveSpacePage()}>
+          <ConvertMove onClick={handleMoveSpacePage}>
             <SquareSpaceIcon />
             {t('CM_GO_SPACES')}
           </ConvertMove>
@@ -480,9 +461,7 @@ const ProfileMyModal = ({
       <Toast
         visible={isToastOpen}
         timeoutMs={1000}
-        onClose={() => {
-          setIsToastOpen(false);
-        }}
+        onClose={() => setIsToastOpen(false)}
       >
         {toastText}
       </Toast>
@@ -516,10 +495,7 @@ const ProfileMyModal = ({
       subContent={subContent}
       footer={
         <UserSettingArea>
-          <SettingButton
-            type="text"
-            onClick={() => handleSettingDialogOpen(SELECTED_TAB.ALARM)}
-          >
+          <SettingButton type="text" onClick={handleSettingDialogOpen}>
             {t('CM_SETTING')}
           </SettingButton>
           {!configStore.isFromCNU ? (
@@ -538,408 +514,5 @@ const ProfileMyModal = ({
     />
   ));
 };
-
-const UserImage = styled.span`
-  display: inline-block;
-  position: relative;
-  width: 3.75rem;
-  height: 3.75rem;
-  border-radius: 50%;
-  background-color: #fff;
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    border: 2px solid rgba(255, 255, 255, 0.2);
-    border-radius: 50%;
-  }
-  img {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-`;
-const UserName = styled.p`
-  overflow: hidden;
-  margin-top: 0.5rem;
-  font-size: 0.88rem;
-  font-weight: 500;
-  line-height: 1.25rem;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-const UserMail = styled.span`
-  display: block;
-  overflow: hidden;
-  margin-top: 0.13rem;
-  font-size: 0.69rem;
-  opacity: 0.8;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-const UserStatus = styled.span`
-  margin-top: 0.3rem;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  word-wrap: break-word;
-  font-size: 0.63rem;
-  color: #fff;
-  opacity: 0.5;
-`;
-const UserButtonBox = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 0.875rem;
-  font-size: 0.69rem;
-  opacity: 0.8;
-  .ant-btn.ant-btn-link {
-    min-width: auto;
-    height: auto;
-    padding: 0 0.375rem;
-    font-size: 0.75rem;
-    line-height: 1.13rem;
-    color: #f7f4ef !important;
-    &:hover span {
-      text-decoration: underline;
-    }
-  }
-`;
-const UserBar = styled.span`
-  display: inline-block;
-  width: 1px;
-  height: 0.81rem;
-  margin: 0 0.375rem;
-  opacity: 0.6;
-  background: #fff;
-`;
-const LogoutButton = styled(Button)`
-  &.ant-btn {
-    margin-top: 0.5rem;
-    width: 100%;
-    color: #f7f4ef;
-    background-color: transparent;
-    border-color: #f7f4ef;
-    &:hover {
-      background-color: rgba(255, 255, 255, 0.2);
-      border-color: #ebe6df;
-      color: #ebe6df;
-    }
-    &:active,
-    &:focus {
-      background-color: #ddd7cd;
-      border-color: #ddd7cd;
-      color: #fff;
-    }
-  }
-`;
-const UserSpaceArea = styled.div`
-  position: relative;
-  padding: 0.625rem 0.5rem 1.06rem 0.875rem;
-  background-color: #fbf9f7;
-  border-radius: 0 0 0.25rem 0.25rem;
-  ${props =>
-    !props.isEdit &&
-    css`
-      &:before {
-        content: '';
-        position: absolute;
-        top: -1rem;
-        left: 0;
-        right: 0;
-        height: 1rem;
-        background-color: #fbf9f7;
-      }
-    `};
-`;
-const DataName = styled.p`
-  margin-bottom: 0.5rem;
-  font-size: 0.63rem;
-  line-height: 0.94rem;
-  color: #777;
-`;
-const DataBox = styled.div`
-  display: flex;
-  align-items: center;
-  .ant-btn {
-    min-width: 1.5rem;
-    width: 1.5rem;
-    height: 1.5rem;
-    border: none;
-    background-color: transparent;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: 1rem 1rem;
-    &:hover {
-      background-color: #f2efec;
-    }
-    &:active,
-    &:hover:active:focus {
-      background-color: #f2efec;
-    }
-  }
-  .btn-convert {
-    position: relative;
-    background-image: url('${convertSpaceIcon}');
-  }
-  .btn-more {
-    margin-left: 0.125rem;
-    background-image: url('${moreSpaceIcon}');
-  }
-`;
-const NewBadge = styled.div`
-  position: absolute;
-  width: 0.5rem;
-  height: 0.5rem;
-  border-radius: 50%;
-  background-color: #dc4547;
-  top: -0.1rem;
-  right: -0.1rem;
-`;
-const Logo = styled(Avatar)`
-  flex-shrink: 0;
-  width: 2.625rem;
-  height: 2.625rem;
-  font-size: 1.125rem;
-  line-height: 2.5rem;
-  font-weight: 500;
-  color: #49423a;
-  border-radius: 0.25rem;
-  background-color: #ebe6df;
-`;
-const Info = styled.p`
-  overflow: hidden;
-  flex: 1;
-  margin: 0 0.625rem;
-  font-size: 0.6875rem;
-  color: #777;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-const Title = styled.strong`
-  overflow: hidden;
-  display: block;
-  font-size: 0.875rem;
-  font-weight: 500;
-  color: #000;
-  line-height: 1.25rem;
-  text-overflow: ellipsis;
-`;
-const Blind = styled.span`
-  position: absolute;
-  clip: rect(0 0 0 0);
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-`;
-const UserSubArea = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 1rem 0.56rem 1rem 1.69rem;
-  color: #000;
-  font-size: 0.81rem;
-  font-weight: 500;
-  & > img {
-    width: 1.25rem;
-    height: 1.25rem;
-    margin-right: 0.63rem;
-  }
-  & + & {
-    border-top: 1px solid #e3e7eb;
-  }
-`;
-const LanguageButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 1.5rem;
-  height: 1.5rem;
-  border-radius: 0.25rem;
-  margin-left: auto;
-  cursor: pointer;
-  &:hover {
-    background-color: #f2efec;
-  }
-`;
-const SubInfo = styled.p`
-  display: flex;
-  align-items: center;
-  position: relative;
-  padding: 0.6875rem 0.875rem;
-  font-size: 0.8125rem;
-  font-weight: 500;
-  line-height: 1.875rem;
-  cursor: pointer;
-  svg {
-    color: #75757f;
-  }
-  & + & {
-    border-top: 1px solid #eeedeb;
-  }
-  &:hover {
-    background-color: #faf8f7;
-    text-decoration: none;
-  }
-  &:active,
-  &:focus {
-    background-color: #f2efec;
-    svg {
-      color: #43434a;
-    }
-  }
-`;
-const ConvertDropdown = styled.div`
-  position: absolute;
-  left: -12.5rem;
-  width: 12rem;
-  top: 15.63rem;
-  border: 1px solid #c6ced6;
-  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.2);
-  border-radius: 0.25rem;
-  background-color: #fff;
-  z-index: 1050;
-`;
-const ConvertNow = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0.3125rem 0.6875rem;
-  margin-top: 0.44rem;
-`;
-const LogoSmall = styled.div`
-  position: relative;
-  width: 2.63rem;
-  height: 2.63rem;
-  font-size: 0.96rem;
-  line-height: 1.96rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.32rem;
-  background-color: #ebe6df;
-  color: #0a1e3a;
-  margin-right: 0.375rem;
-  ${props =>
-    props.checked &&
-    css`
-      line-height: 1.625rem;
-      border: 1px solid #0a1e3a;
-    `}
-`;
-const LogoNumber = styled.div`
-  position: absolute;
-  line-height: 1.4;
-  top: 0.15rem;
-  right: 0.15rem;
-  background-color: rgb(220, 69, 71);
-  color: rgb(255, 255, 255);
-  font-size: 0.52rem;
-  font-weight: 400;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.4rem;
-  padding: 0 0.2rem;
-`;
-const NowInfo = styled(Info)`
-  margin: 0 0.375rem 0 0;
-  font-size: 0.625rem;
-  line-height: 0.9375rem;
-`;
-const NowTitle = styled(Title)`
-  font-size: 0.75rem;
-  line-height: 1.125rem;
-`;
-const ConvertList = styled.ul`
-  overflow-y: auto;
-  max-height: 11.25rem;
-`;
-const ConvertItem = styled.li`
-  display: flex;
-  align-items: center;
-  padding: 0.3125rem 0.6875rem;
-  color: #000;
-  cursor: pointer;
-  .ant-avatar {
-    margin-right: 0.375rem;
-  }
-  &:hover {
-    background-color: #faf8f7;
-  }
-`;
-const ItemText = styled.p`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  font-size: 0.75rem;
-  font-weight: 500;
-  color: ${props => (props.addSpace ? '#666666' : '#000000')};
-`;
-const ConvertBox = styled.div`
-  padding: 0.31rem 0;
-`;
-const ConvertAdd = styled.div`
-  display: flex;
-  padding: 0.3125rem 0.6875rem;
-  align-items: center;
-  font-weight: 500;
-  cursor: pointer;
-`;
-const AddBox = styled.span`
-  height: 2.63rem;
-  width: 2.63rem;
-  background-color: #faf8f7;
-  border-radius: 0.32rem;
-  margin-right: 0.375rem;
-  font-size: 0.88rem;
-  line-height: 1.75rem;
-  color: #49423a;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const AddText = styled.span`
-  font-size: 0.75rem;
-  color: #666;
-`;
-const ConvertMove = styled.div`
-  padding: 0.63rem 1.13rem;
-  border-top: 1px solid #eeedeb;
-  font-size: 0.69rem;
-  font-weight: 500;
-  color: #00493d;
-  cursor: pointer;
-  svg {
-    margin-right: 0.4rem;
-    width: 1rem;
-    height: 1rem;
-    vertical-align: top;
-  }
-`;
-const UserSettingArea = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.625rem 0;
-  border-top: 1px solid #eeedeb;
-`;
-const SettingButton = styled(Button)`
-  min-width: 4.375rem;
-`;
-const SettingBar = styled.span`
-  display: inline-block;
-  width: 0.1875rem;
-  height: 0.1875rem;
-  margin: 0 0.375rem;
-  background-color: #7b7671;
-  border-radius: 50%;
-`;
 
 export default ProfileMyModal;
