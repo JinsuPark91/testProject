b44a699bf629c8eca76f0e467cc0c656ca3983d6
홍석종 <seokjong_hong@tmax.co.kr>
2021-04-26 12:52:29 +0900

    fix: window 먼저 체크하도록 수정

--- src/page/PrivateLoginPage.js
+++ src/page/PrivateLoginPage.js
@@ -37,10 +37,9 @@ function PrivateLoginPage() {
         domainUrl: '',
         authorizeType: 'Ksign',
         ssoType:
-          process.env.REACT_APP_SSO_TYPE || window?.env?.REACT_APP_SSO_TYPE,
+          window?.env?.REACT_APP_SSO_TYPE || process.env.REACT_APP_SSO_TYPE,
       });
       if (res.id) {
-       
         if (window.location.pathname.includes('/mobile')) {
           history.push(`/friend`);
         } else {
