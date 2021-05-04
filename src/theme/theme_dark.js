d6665e34b255b294098383802ad1f2c06511a385
kimchaelin <chaelin_kim@tmax.co.kr>
2021-05-04 05:05:33 +0000

    chore: 테마 색상

--- src/theme/theme_dark.js
+++ src/theme/theme_dark.js
@@ -1,8 +1,20 @@
 /* eslint-disable import/prefer-default-export */
 
 const colors = {
-  name: 'dark',
-  bg_color: '#000000',
+  StateBeigeColor: '#0A0A0D',
+  StateBeigeBrightColor: '#191A1B',
+  StateBeigeLightColor: '#2D2F32',
+  StateBeigeDarkColor: '#222325',
+
+  WapleBeigeColor: '#7B7671',
+  WapleBeigeBrightColor: '#645F5B',
+  WapleBeigeLightColor: '#5C5753',
+  WapleBeigeDarkColor: '#4B4744',
+
+  WapleNavyColor: '#171819',
+  WapleNavyBrightColor: '#191A1B',
+  WapleNavyLightColor: '#3B3C3F',
+  WapleNavyDarkColor: '#313438',
 };
 
 export { colors };
