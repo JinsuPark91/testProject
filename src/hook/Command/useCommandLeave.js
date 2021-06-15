a020dc5b32a87aa75ca3c8d04e1385d448d1ba43
Joonhee Min <joonhee_min@tmax.co.kr>
2021-06-15 01:29:38 +0000

    feat: unmute 커맨드 생성

--- src/hook/Command/useCommandLeave.js
+++ src/hook/Command/useCommandLeave.js
@@ -4,7 +4,7 @@ import { autorun } from 'mobx';
 import { useCoreStores } from 'teespace-core';
 import { useStores } from '../../stores';
 
-export const useCommandMute = () => {
+export const useCommandLeave = () => {
   const { commandStore, roomStore } = useCoreStores();
   const { handlerStore, uiStore } = useStores();
   const handler = useCallback(
@@ -14,25 +14,22 @@ export const useCommandMute = () => {
   );
 
   useEffect(() => {
-    const command = '/mute';
-
+    const command = '/leave';
     autorun(() => {
       const roomInfo = roomStore.getRoom(uiStore.resourceId);
       const isMyroom = roomInfo?.type === 'WKS0001';
-      const isAlarmUsed = !!roomInfo?.isAlarmUsed;
-
-      if (isAlarmUsed && !isMyroom && !commandStore.get(command)) {
+      if (!isMyroom && !commandStore.get(command)) {
         commandStore.register('platform', command, handler, {
           desc: {
-            ko: '현재 룸의 알림을 끄세요.',
-            en: 'Turn off notification for the current room.',
+            // TODO: ui string 발간 이후 수정
+            ko: '지금 있는 방에서 나갑니다.',
+            en: 'Leave from this room.',
           },
         });
-      } else if ((!isAlarmUsed || isMyroom) && commandStore.get(command)) {
+      } else if (isMyroom && commandStore.get(command)) {
         commandStore.unregister(command);
       }
     });
-
     return () => commandStore.get(command) && commandStore.unregister(command);
   }, [commandStore, handler, roomStore, uiStore]);
 };
