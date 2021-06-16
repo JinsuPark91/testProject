81c065e4b64903d02d7f1f7af25f638f2dd593c6
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2021-06-16 17:39:14 +0900

    fix: profile에서 1:1talk  누를때, 탭 아이콘 보이지 않는 현상 수정

--- src/components/main/LeftSide.js
+++ src/components/main/LeftSide.js
@@ -52,128 +52,144 @@ const LeftSide = () => {
 
   const themeContext = useContext(ThemeContext);
 
+  const FriendTabPane = useMemo(() => (
+    <TabPane
+      key="f"
+      tab={
+        <Tooltip
+          title={t('CM_FRIENDS_LIST')}
+          placement="bottom"
+          color={themeContext.CoreLight}
+        >
+          <IconWrapper className="lnb__icon-wrapper lnb__friends">
+            <Observer>
+              {() => (
+                <UnreadCount isVisible={uiStore.newFriendCount > 0}>
+                  {uiStore.newFriendCount}
+                </UnreadCount>
+              )}
+            </Observer>
+
+            <Observer>
+              {() =>
+                uiStore.tabType === 'f' ? (
+                  <PeopleActiveIcon
+                    width={1.5}
+                    height={1.5}
+                    color={themeContext.BasicDark}
+                  />
+                ) : (
+                  <PeopleIcon width={1.5} height={1.5} />
+                )
+              }
+            </Observer>
+          </IconWrapper>
+        </Tooltip>
+      }
+    >
+      <FriendLnb />
+    </TabPane>
+  ));
+
+  const RoomTabPane = useMemo(() => (
+    <TabPane
+      key="s"
+      tab={
+        <Tooltip
+          title={t('CM_COMMUNICATION_BAR_02')}
+          placement="bottom"
+          color={themeContext.CoreLight}
+        >
+          <IconWrapper className="lnb__icon-wrapper lnb__rooms">
+            <Observer>
+              {() => (
+                <UnreadCount isVisible={uiStore.totalUnreadCount > 0}>
+                  {uiStore.totalUnreadCount > 99
+                    ? '99+'
+                    : uiStore.totalUnreadCount}
+                </UnreadCount>
+              )}
+            </Observer>
+
+            <Observer>
+              {() =>
+                uiStore.tabType === 's' ? (
+                  <ChattingActiveIcon
+                    width={1.5}
+                    height={1.5}
+                    color={themeContext.BasicDark}
+                  />
+                ) : (
+                  <ChattingIcon width={1.5} height={1.5} />
+                )
+              }
+            </Observer>
+          </IconWrapper>
+        </Tooltip>
+      }
+    >
+      <RoomList />
+    </TabPane>
+  ));
+
+  const MailTabPane = useMemo(() =>
+    configStore.isActivateForCNU('Mail') ? (
+      <TabPane
+        key="m"
+        tab={
+          <Tooltip
+            title={t('CM_COMMUNICATION_BAR_03')}
+            placement="bottom"
+            color={themeContext.CoreLight}
+          >
+            <IconWrapper className="lnb__icon-wrapper lnb__mail">
+              <Observer>
+                {() => (
+                  <UnreadCount isVisible={MailStore.unreadTotalCount > 0}>
+                    {MailStore.unreadTotalCount > 99
+                      ? '99+'
+                      : MailStore.unreadTotalCount}
+                  </UnreadCount>
+                )}
+              </Observer>
+
+              <Observer>
+                {() =>
+                  uiStore.tabType === 'm' ? (
+                    <MailActiveIcon
+                      width={1.5}
+                      height={1.5}
+                      color={themeContext.BasicDark}
+                    />
+                  ) : (
+                    <MailIcon width={1.5} height={1.5} />
+                  )
+                }
+              </Observer>
+            </IconWrapper>
+          </Tooltip>
+        }
+      >
+        <MailSideView language={i18n.language} />
+      </TabPane>
+    ) : null,
+  );
+
   return (
     <Wrapper>
-      <CustomTabs
-        defaultActiveKey={uiStore.tabType}
-        onTabClick={handleSelectTab}
-        animated={false}
-      >
-        <TabPane
-          key="f"
-          tab={
-            <Tooltip
-              title={t('CM_FRIENDS_LIST')}
-              placement="bottom"
-              color={themeContext.CoreLight}
-            >
-              <IconWrapper className="lnb__icon-wrapper lnb__friends">
-                <Observer>
-                  {() => (
-                    <UnreadCount isVisible={uiStore.newFriendCount > 0}>
-                      {uiStore.newFriendCount}
-                    </UnreadCount>
-                  )}
-                </Observer>
-
-                <Observer>
-                  {() =>
-                    uiStore.tabType === 'f' ? (
-                      <PeopleActiveIcon
-                        width={1.5}
-                        height={1.5}
-                        color={themeContext.BasicDark}
-                      />
-                    ) : (
-                      <PeopleIcon width={1.5} height={1.5} />
-                    )
-                  }
-                </Observer>
-              </IconWrapper>
-            </Tooltip>
-          }
-        >
-          <FriendLnb />
-        </TabPane>
-        <TabPane
-          key="s"
-          tab={
-            <Tooltip
-              title={t('CM_COMMUNICATION_BAR_02')}
-              placement="bottom"
-              color={themeContext.CoreLight}
-            >
-              <IconWrapper className="lnb__icon-wrapper lnb__rooms">
-                <Observer>
-                  {() => (
-                    <UnreadCount isVisible={uiStore.totalUnreadCount > 0}>
-                      {uiStore.totalUnreadCount > 99
-                        ? '99+'
-                        : uiStore.totalUnreadCount}
-                    </UnreadCount>
-                  )}
-                </Observer>
-
-                <Observer>
-                  {() =>
-                    uiStore.tabType === 's' ? (
-                      <ChattingActiveIcon
-                        width={1.5}
-                        height={1.5}
-                        color={themeContext.BasicDark}
-                      />
-                    ) : (
-                      <ChattingIcon width={1.5} height={1.5} />
-                    )
-                  }
-                </Observer>
-              </IconWrapper>
-            </Tooltip>
-          }
-        >
-          <RoomList />
-        </TabPane>
-        {configStore.isActivateForCNU('Mail') ? (
-          <TabPane
-            key="m"
-            tab={
-              <Tooltip
-                title={t('CM_COMMUNICATION_BAR_03')}
-                placement="bottom"
-                color={themeContext.CoreLight}
-              >
-                <IconWrapper className="lnb__icon-wrapper lnb__mail">
-                  <Observer>
-                    {() => (
-                      <UnreadCount isVisible={MailStore.unreadTotalCount > 0}>
-                        {MailStore.unreadTotalCount > 99
-                          ? '99+'
-                          : MailStore.unreadTotalCount}
-                      </UnreadCount>
-                    )}
-                  </Observer>
-
-                  <Observer>
-                    {() =>
-                      uiStore.tabType === 'm' ? (
-                        <MailActiveIcon
-                          width={1.5}
-                          height={1.5}
-                          color={themeContext.BasicDark}
-                        />
-                      ) : (
-                        <MailIcon width={1.5} height={1.5} />
-                      )
-                    }
-                  </Observer>
-                </IconWrapper>
-              </Tooltip>
-            }
+      <Observer>
+        {() => (
+          <CustomTabs
+            activeKey={uiStore.tabType}
+            onTabClick={handleSelectTab}
+            animated={false}
           >
-            <MailSideView language={i18n.language} />
-          </TabPane>
-        ) : null}
-      </CustomTabs>
+            {FriendTabPane}
+            {RoomTabPane}
+            {MailTabPane}
+          </CustomTabs>
+        )}
+      </Observer>
     </Wrapper>
   );
 };
