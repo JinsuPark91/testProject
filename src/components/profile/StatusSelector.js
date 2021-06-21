933640d7b5559d2816fcc1e6deb706eb64fbc924
jinsu_park <jinsu_park@tmax.co.kr>
2021-06-21 16:18:19 +0900

    fix(selector): 스타일 수정, 동작 수정

--- src/components/profile/StatusSelector.js
+++ src/components/profile/StatusSelector.js
@@ -1,20 +1,22 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Dropdown, Button, Menu } from 'antd';
-import 'antd/dist/antd.css';
 import { Observer } from 'mobx-react';
 import { Icons, useCoreStores } from 'teespace-core';
 import { useStores } from '../../stores';
 
-const StatusSelector = () => {
+const StatusSelector = ({ editable = false }) => {
   const { uiStore } = useStores();
   const { userStore } = useCoreStores();
+  const [isSelected, setIsSelected] = useState(false);
+
   const {
     ProfileEmoticonEmptyIcon,
     ProfileEmoticonNormalIcon,
     ProfileEmoticonMissedIcon,
     ProfileEmoticonVacationIcon,
     ProfileEmoticonMeetingIcon,
+    CommonAddIcon,
   } = Icons;
   const statusMap = {
     '상태 없음': 'STA0000',
@@ -24,25 +26,42 @@ const StatusSelector = () => {
     회의중: 'STA0004',
   };
 
+  const handleVisibleChange = visible => {
+    if (visible) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  };
+
   useEffect(() => {
     const code = userStore.myProfile.status;
     uiStore.setStatusCode(code);
   }, []);
 
-  const renderIcon = code => {
+  const renderIcon = (code, isForSelector) => {
     if (code === 'STA0001') {
-      return <ProfileEmoticonNormalIcon width={1.2} height={1.2} />;
+      return <ProfileEmoticonNormalIcon width={0.875} height={0.875} />;
     }
     if (code === 'STA0002') {
-      return <ProfileEmoticonMissedIcon width={1.2} height={1.2} />;
+      return <ProfileEmoticonMissedIcon width={0.875} height={0.875} />;
     }
     if (code === 'STA0003') {
-      return <ProfileEmoticonVacationIcon width={1.2} height={1.2} />;
+      return <ProfileEmoticonVacationIcon width={0.875} height={0.875} />;
     }
     if (code === 'STA0004') {
-      return <ProfileEmoticonMeetingIcon width={1.2} height={1.2} />;
+      return <ProfileEmoticonMeetingIcon width={0.875} height={0.875} />;
     }
-    return <ProfileEmoticonEmptyIcon width={1.2} height={1.2} />;
+    if (isForSelector) {
+      return (
+        <CommonAddIcon
+          width={0.875}
+          height={0.875}
+          color={isSelected ? '#000' : '#fff'}
+        />
+      );
+    }
+    return <ProfileEmoticonEmptyIcon width={0.875} height={0.875} />;
   };
 
   const statusArray = Object.keys(statusMap);
@@ -52,10 +71,9 @@ const StatusSelector = () => {
       {statusArray.map(status => {
         const statusCode = statusMap[status];
         const handleClick = async () => {
-          await userStore.updateProfileStatus({ status: statusCode });
           uiStore.setStatusCode(statusCode);
-          console.log(status);
-          console.log(statusCode);
+          setIsSelected(false);
+          await userStore.updateProfileStatus({ status: statusCode });
         };
         return (
           <MenuItem key={status} onClick={handleClick}>
@@ -70,44 +88,100 @@ const StatusSelector = () => {
   return (
     <Observer>
       {() => (
-        <Dropdown overlay={menu} trigger={['click']} placement="bottomLeft">
-          <StyledButton icon={renderIcon(uiStore.statusCode)}>
-            {uiStore.statusText}
-          </StyledButton>
-        </Dropdown>
+        <SelectorWrapper>
+          {editable && (
+            <Dropdown
+              overlay={menu}
+              trigger={['click']}
+              placement="bottomLeft"
+              onVisibleChange={handleVisibleChange}
+            >
+              <Wrapper
+                className={
+                  isSelected
+                    ? 'selected selector-editable'
+                    : 'selector-editable'
+                }
+              >
+                {renderIcon(uiStore.statusCode, true)}
+                <StyledButton>{uiStore.statusText}</StyledButton>
+              </Wrapper>
+            </Dropdown>
+          )}
+          {!editable && (
+            <Wrapper>
+              {renderIcon(uiStore.statusCode, true)}
+              <StyledButton>{uiStore.statusText}</StyledButton>
+            </Wrapper>
+          )}
+        </SelectorWrapper>
       )}
     </Observer>
   );
 };
 
-const StyledButton = styled(Button)`
+const SelectorWrapper = styled.div`
+  position: absolute;
+  top: 13.45rem;
+  z-index: 100;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-width: 0px;
+  border-radius: 4px;
+  font-size: 0.688rem;
+  color: #ffffff;
   width: 6rem;
   height: 1.438rem;
-  border-radius: 4px;
-  color: #ffffff;
   background: rgba(255, 255, 255, 0.1);
+  &.selector-editable {
+    cursor: pointer;
+    &:hover {
+      background: rgba(250, 248, 247, 0.3);
+    }
+    &.selected {
+      background: rgba(242, 239, 236, 0.5);
+      color: #000000;
+    }
+  }
+`;
+
+const StyledButton = styled.div`
+  margin-left: 0.375rem;
 `;
 
 const MenuText = styled.span`
   width: 2.688rem;
   height: 1.25rem;
-  line-height: 1.25rem;
-  font-family: NotoSansCJKkr;
+  margin-left: 0.375rem;
   font-size: 0.688rem;
   color: #ffffff;
 `;
 
-const MenuItem = styled(Menu.Item)`
-  line-height: 1.25rem;
-`;
+const MenuItem = styled(Menu.Item)``;
 
 const NewMenu = styled(Menu)`
-  left: 6rem;
-  width: 5.188rem;
-  height: 8.75rem;
+  left: 6.25rem;
+  top: -1.6rem;
+  width: 6rem;
   border-radius: 4px;
   background: rgba(255, 255, 255, 0.2);
+  border-width: 0px;
+  & > .ant-dropdown-menu-item {
+    margin: 0.187rem 0;
+  }
+  & .ant-dropdown-menu-item:hover {
+    background: rgba(250, 248, 247, 0.3);
+  }
+  & .ant-dropdown-menu-item:active {
+    background: rgba(242, 239, 236, 0.5);
+    & span {
+      color: #000;
+    }
+  }
 `;
 
 export default StatusSelector;
